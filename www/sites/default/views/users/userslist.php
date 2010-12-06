4991e651117d234853e4a2c3362c1d05b3c62d80
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2010-12-06 17:29:06 +0000

    users are linked to the single page.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@41 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/users/userslist.php
+++ www/sites/default/views/users/userslist.php
@@ -3,7 +3,7 @@
    <?$count = $offset + 1;?>
 
         <?php foreach ($users as $user): ?>
-          <tr><td><?= $count; $count++;?><td><?= $user->username ?></td><td><?= $user->email ?></td></tr>
+          <tr><td><?= $count; $count++;?><td><a href="admin/users/single/<?= $user->id ?>"><?= $user->username ?></a></td><td><?= $user->email ?></td></tr>
         <?php endforeach; ?>
 </table>
 

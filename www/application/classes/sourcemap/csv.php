c90dd8da5072ba98d4111c78f3c994182f2eec23
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-01-07 14:15:54 +0000

    csv class
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@71 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/application/classes/sourcemap/csv.php
@@ -0,0 +1,95 @@
+<?php
+class Sourcemap_Csv {
+
+    const FSTA = 'FSTA';
+    const FEND = 'FEND';
+    const FVAL = 'FVAL';
+    const FESC = 'FESC';
+    const FQUO = 'FQUO';
+
+    public static function parse_csv_row($row) {
+        $row = trim($row);
+        $vs = array();
+        $b = '';
+        $st = self::FSTA;
+        for($i=0; $i<strlen($row); $i++) {
+            $c = $row[$i];
+            switch($st) {
+                case self::FSTA:
+                    switch($c) {
+                        case '"':
+                            $st = self::FQUO;
+                            break;
+                        case ',':
+                            $vs[] = '';
+                            break;
+                        default:
+                            $b .= $c;
+                            $st = self::FVAL;
+                            break;
+                    }
+                    break;
+                case self::FQUO:
+                    switch($c) {
+                        case '"':
+                            $st = self::FEND;
+                            break;
+                        case '\\':
+                            $st = self::FESC;
+                            break;
+                        default:
+                            $b .= $c;
+                            break;
+                    }
+                    break;
+                case self::FVAL:
+                    switch($c) {
+                        case ',':
+                            $vs[] = $b;
+                            $b = '';
+                            $st = self::FSTA;
+                            break;
+                        default:
+                            $b .= $c;
+                            break;
+                    }
+                    break;
+                case self::FESC:
+                    switch($c) {
+                        case '"':
+                            $b .= $c;
+                            break;
+                        default:
+                            $b .= '\\';
+                            $b .= $c;
+                            break;
+                    }
+                    break;
+                case self::FEND:
+                    switch($c) {
+                        case ',':
+                            $vs[] = $b;
+                            $b = '';
+                            $st = FSTA;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        if($b) $vs[] = $b;
+        return $vs;
+    }
+
+    function make_csv_row($arr, $delim=',', $encap='"') {
+        $csv_arr = array();
+        foreach($arr as $i => $s) {
+            if($encap) $s = str_replace($encap, '\\'.$encap, $s);
+            $csv_arr[] = (string)$s == '' ? '' : $encap.$s.$encap;
+        }
+        return implode($delim, $csv_arr);
+    }
+}

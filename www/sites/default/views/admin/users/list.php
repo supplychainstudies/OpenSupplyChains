9aeff8adc0260e58e2474e3a997f1284391acbad
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-01-17 20:08:03 +0000

    admin
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@87 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/sites/default/views/admin/users/list.php
@@ -0,0 +1,11 @@
+<fieldset>
+<legend>Create a new user:</legend>
+<form name="create-user" method="post" action="admin/users/create">
+<label for="email-address">Email:</label><input type="text" name="email-address" />&nbsp;<label for="username">Username:</label><input type="text" name="username" />
+<input type="submit" value="create" name="create-user" />
+</form>
+</fieldset>
+<?= View::factory('partial/admin/list', array('list' => $users, 'list_type' => 'users')) ?>
+<?php echo $page_links; ?>
+
+    

4aec9f8c525e483e6406f73ba0b3812a5143a4fa
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-01-19 04:53:30 +0000

    View Scaffolding...
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@89 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/assets/scripts/script.js
@@ -0,0 +1,49 @@
+/* Author: 
+<link rel="shortcut icon" href="../images/favicon.ico">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+*/
+
+
+function toggleDetailPane() {
+    var left = (parseInt($("#detail-pane").css('left'),10) == 5 ? 220 : 5);
+    $("#detail-pane").css("left", left);
+    var width = (parseInt($("#detail-pane").css('width'),10) == 200 ? 350 : 200);
+    $("#detail-pane").css("width", width);
+}
+
+jQuery(document).ready(function(){
+
+    $( "#view-pane" ).accordion();
+    $(".place-details, #detail-pane .close").click(function() {toggleDetailPane();});
+    $("#map").click(function() {
+        if($("#map").css("position") == "fixed") {
+            $("#map").css("z-index", "").css("position","static").css("height","600px")
+                     .css("left","").css("top","").css("width","100%").css("margin-left","221px")
+                     .css("-webkit-box-shadow","inset 5px 5px 5px rgba(0, 0, 0, 0.2)");
+            $("#map").css("width",(parseInt($("#map").css('width'),10)-221));
+        } else {
+            $("#map").css("z-index", 100).css("position","fixed").css("width","100%")
+                     .css("height","100%").css("left",0).css("top",0).css("margin",0)
+                     .css("-webkit-box-shadow","none");
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- www/sites/default/views/layout/layout.php
+++ www/sites/default/views/layout/layout.php
@@ -1,19 +1,139 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>  
+
+<html lang="en" class="no-js">
 <head>
-<base href="<?= URL::base() ?>" />
-<title><?= HTML::chars(isset($page_title) && $page_title ? $page_title : APPLONGNM) ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <base href="<?= URL::base() ?>" />
+    <title><?= HTML::chars(isset($page_title) && $page_title ? $page_title : APPLONGNM) ?></title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+    <link rel="stylesheet" href="<?= URL::base() ?>assets/styles/style.css?v=2">
+    <link rel="stylesheet/less" href="<?= URL::base() ?>assets/styles/sourcemap.less?v=2" type="text/css">
+
+    <script src="<?= URL::base() ?>assets/scripts/libs/modernizr-1.6.min.js"></script>
+    <script src="<?= URL::base() ?>assets/scripts/libs/less-1.0.40.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.7/jquery-ui.min.js"></script>
+
 </head>
-<body>
-<div id="masthead">
-<h1><?= HTML::chars(isset($page_title) && $page_title ? $page_title : APPLONGNM) ?></h1>
-<?php if(Breadcrumbs::instance()->get()): ?><?= Breadcrumbs::instance()->render() ?><?php endif; ?>
-<?php if(Message::instance()->get()): ?><?= Message::instance()->render() ?><?php endif; ?>
-</div>
-<div id="main">
-<?= $content ?>
-</div>
-<div id="footer">
-</div>
+
+<body id="supplychain">
+    <div id="branding">
+        <header id="masthead">
+            <h1>Sourcemap</h1>
+        </header>
+        <nav id="main-navigation">
+            <ul class="nav">
+                <li class="register-link"><a href="">Join us</a> or <a href="">Log in</a></li>          
+                <li id="browse-navigation">
+                    <h3>Browse</h3>            
+                    <ul>
+                        <li><a href="">Sourcemaps</a></li>
+                        <li>
+                            <a href="">Carbon Catalogue</a>
+                            <ul>
+                                <li><a href="">Part Catalogue</a></li>
+                                <li><a href="">Transport Cat.</a></li>
+                                <li><a href="">Power Cat.</a></li>
+                                <li><a href="">Process Cat.</a></li>
+                                <li><a href="">Endoflife Cat.</a></li>                                                                     
+                            </ul>
+                        </li>
+                        <li><a href="">Members</a></li>      
+                        <li><a href="">Groups</a></li>                    
+                                      
+                    </ul>
+                </li>
+                <li id="info-navigation">
+                    <h3>Info</h3>
+                    <ul>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Who We Are</a></li>
+                        <li><a href="">Join Us!</a></li>
+                        <li><a href="">API and Code</a></li>
+                        <li>
+                            <a href="">Help</a>
+                            <ul>
+                                <li><a href="">Data References</a></li>
+                                <li><a href="">FAQs</a></li>                                    
+                            </ul>                                
+                        </li>
+                        <li><a href="">Get In Touch</a></li>                    
+                    </ul>
+                </li>
+            </ul>
+            <div id="search"><input id="search-field" type="search" results="0" placeholder="Search" /></div>
+        </nav>
+    </div> <!-- branding -->
+    <div class="clear"></div>
+   
+    <div id="bar">
+        <nav id="page-navigation">
+            <h2>Generic Page Title</h2>
+            <div class="clear"></div>
+        </nav>
+    </div> <!-- bar -->
+    <div id="content">          
+         <article>
+             <header>
+                 <h2><?= HTML::chars(isset($page_title) && $page_title ? $page_title : APPLONGNM) ?></h2>
+                 <p><?php if(Breadcrumbs::instance()->get()): ?><?= Breadcrumbs::instance()->render() ?><?php endif; ?></p><?php if(Message::instance()->get()): ?><?= Message::instance()->render() ?><?php endif; ?>
+                 
+             </header>
+             <p><?= $content ?></p>
+             
+             <footer></footer>
+         </article>
+         <aside id="sidebar">
+             <h3></h3>
+             <p></p>
+         </aside>
+         <nav id="secondary-navigation">
+        
+         </nav>
+    </div>
+    <footer id="footer">
+        <div id="footer-callouts">
+            <div class="footer-box">
+                <h3>Contact us</h3>
+                <p>Interested in helping? Partnering? Just have some questions? Contact us at <a>info[at]sourcemap[dot]org</a>.</p> 
+            </div>
+            <div class="footer-box">               
+                <h3>Creative Commons</h3>
+                <p>All of our user generated content (maps, comments, etc.) is licensed <a href="">BY-SA Creative commons 3.0</a>.</p> 
+            </div>
+            <div class="footer-box">               
+                <h3>Open Data</h3>
+                <p>Sourcemap is committed to open data. All of our data is <a href="">PDDL (1.0)</a> and available at <a href="">data.sourcemap.org</a>.</p> 
+            </div>
+            <div class="clear"></div>
+            <p>Sourcemap (TM). For more information on how we use your data, read our <a href="">privacy policy</a>. For more information on how you can use the site, read our <a href="">terms of service</a>.</p>
+        </div>
+        <div id="footer-about">
+            <h3>About Sourcemap</h3>
+            <p>We built Sourcemap so that everyone can access the information needed to make sustainable choices and share them with the world. The project is free, opensource, and volunteer driven. For more news, check the <a href="">Sourcemap Blog</a>.</p>
+        </div>    
+        <div class="clear"></div>
+    </footer>
+</body>
+
+  
+  <script src="js/script.js"></script>
+  
+  
+  <!--[if lt IE 7 ]>
+    <script src="js/libs/dd_belatedpng.js"></script>
+    <script> DD_belatedPNG.fix('img, .png_bg'); 
+  <![endif]-->
+  
 </body>
 </html>

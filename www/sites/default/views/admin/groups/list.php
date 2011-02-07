5666f30871662e00fcb377ff0e112184e62d2c09
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-07 19:59:36 +0000

    list_type groups.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@166 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/admin/groups/list.php
+++ www/sites/default/views/admin/groups/list.php
@@ -1,10 +1,11 @@
 <fieldset>
 <legend>Create a new group:</legend>
-<form name="create-user" method="post" action="">
+<form name="create-group" method="post" action="admin/groups/create_group">
 <label for="username">Username:</label><input type="text" name="username" />&nbsp;<label for="groupname">Groupname:</label><input type="text" name="groupname" />
 <input type="submit" value="create" name="create-group" />
 </form>
 </fieldset>
+
 <?= View::factory('partial/admin/list', array('list' => $groups, 'list_type' => 'groups')) ?>
 <?php echo $page_links; ?>
 

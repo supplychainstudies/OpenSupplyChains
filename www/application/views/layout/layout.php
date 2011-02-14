42c8a1f5fc7e7b8bbad4f219025f99a30c12c5b2
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-14 23:33:43 +0000

    sf demo and scaffolding
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@186 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/application/views/layout/layout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>  
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <base href="<?= URL::base(true, true) ?>"></base>
+    <title><?= HTML::chars(isset($page_title) && $page_title ? $page_title : APPLONGNM) ?></title>
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+    <?php if(isset($styles)): ?>
+        <?= Sourcemap_CSS::link_tags($styles) : '' ?>
+    <?php endif; ?>
+</head>
+<body>
+<?= isset($content) ? $content : '<h2>There\'s nothing here.</h2>' ?> 
+</body>
+</html>

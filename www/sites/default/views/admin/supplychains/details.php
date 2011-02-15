44289ba5036b584580eef232228a70bd677ca885
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-15 19:49:21 +0000

    Sites and alias listing and creating alias.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@193 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/admin/supplychains/details.php
+++ www/sites/default/views/admin/supplychains/details.php
@@ -1,5 +1,34 @@
+<form name="alias-info" method="post" action="">
+<label for="site">site:</label>
+<input type="text" name="site" class="input text" value=""/>
+
+<label for="alias">alias:</label>
+<input type="text" name="alias" class="input text" value=""/>
+<input type="submit" value="Create Alias" />
+</form><br />
+
+
 <strong>Number of stops:</strong> <?=$stop_count;?><br />
 <strong>Number of hops:</strong> <?=$hop_count;?><br />
 <?php if(!empty($attribute_key)): ?>
 <strong>Attribute:</strong> <?=$attribute_key;?> 
  <?php endif;?><br />
+
+<table>
+<tr>
+<th>Site</th>
+<th>Alias</th>
+</tr>
+<?php foreach($alias as $alias_name): ?>
+<tr>
+<td><?=$alias_name['site'];?></td>
+<td><?=$alias_name['alias'];?></td>
+</tr>
+<?php endforeach;?>
+</table> 
+
+
+
+
+
+

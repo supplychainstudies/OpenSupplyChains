4f0b34829f2a33bf7a970571797e8b0fedc4ac04
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-15 20:33:46 +0000

    delete alias functionality.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@194 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/classes/controller/admin/supplychains.php
+++ www/sites/default/classes/controller/admin/supplychains.php
@@ -72,7 +72,7 @@ class Controller_Admin_Supplychains extends Controller_Admin {
 
 	$attribute= $supplychain->attributes->find_all()->as_array(null, 'key');
 
-	$alias = $supplychain->alias->find_all()->as_array(null, array('site', 'alias'));
+	$alias = $supplychain->alias->find_all()->as_array(null, array('supplychain_id', 'site', 'alias'));
 
 	$this->template->stop_count = $stop_count;
 	$this->template->hop_count = $hop_count;
@@ -127,4 +127,28 @@ class Controller_Admin_Supplychains extends Controller_Admin {
             ->add(ucwords($attribute[0]), 'admin/supplychains/'.$id);
 	
     }
+
+
+    public function action_delete_alias($id) {
+	$post = Validate::factory($_POST);
+        $post->rule('alias', 'not_empty')
+	    ->rule('site', 'not_empty')
+            ->filter(true, 'trim');
+        if(strtolower(Request::$method) === 'post' && $post->check()) {
+            $post = (object)$post->as_array();
+            $alias = $post->alias;  
+	    $site = $post->site;  
+            $supplychain_alias= ORM::factory('supplychain_alias', array('site' => $site, 'alias' => $alias, 'supplychain_id' => $id));
+	    $supplychain_alias->delete();	    
+
+        } elseif(strtolower(Request::$method === 'post')) {
+            Message::instance()->set('Could not delete role.', Message::ERROR);
+        } else {
+            Message::instance()->set('Bad request.');
+        }
+
+        $this->request->redirect("admin/supplychains/".$id);
+    }
 }
+
+

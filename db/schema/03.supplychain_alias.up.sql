f9ab7f61ac29570f331a7de34fb6d8e2e2adeb67
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-15 17:40:09 +0000

    added pk in sc_alias table
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@191 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- db/schema/03.supplychain_alias.up.sql
+++ db/schema/03.supplychain_alias.up.sql
@@ -3,9 +3,10 @@ create table supplychain_alias (
     supplychain_id integer not null,
     site varchar(32) not null default 'default',
     alias varchar(32) not null,
+    constraint supplychain_alias_id_pkey primary key (id),
     constraint supplychain_alias_key unique (site, alias)
 );
 
 insert into sourcemap_schema_version ("key", extra) values (
-    '03.initial', 'Supplychain aliases.'
+    '03.supplychain_alias', 'Supplychain aliases.'
 );

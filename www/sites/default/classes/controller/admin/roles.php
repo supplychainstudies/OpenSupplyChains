90bd1980d884558e740838b25927bbf06d3bbeb9
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-16 19:47:14 +0000

    fixed create_role.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@216 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/classes/controller/admin/roles.php
+++ www/sites/default/classes/controller/admin/roles.php
@@ -15,38 +15,56 @@ class Controller_Admin_Roles extends Controller_Admin {
 
 
     public function action_index() {
-
-	    $role = ORM::factory('role');
-	    $page = max($this->request->param('page'), 1);
-	    $items = 5;
-	    $offset = ($items * ($page - 1));
-	    $count = $role->count_all();
-	    $pagination = Pagination::factory(array(
-						  'current_page' => array(
-						      'source' => 'query_string', 
-						      'key' => 'page'
-						      ),
-						  'total_items' => $role->count_all(),
-						  'items_per_page' => $items,
-						  ));
-	    $this->template->roles = $role->order_by('name', 'ASC')
-		->limit($pagination->items_per_page)
-		->offset($pagination->offset)
-		->find_all()->as_array(null, array('id', 'name', 'description'));
-	    $this->template->page_links = $pagination->render();
-	    $this->template->offset = $pagination->offset;
-	    
-	    $role_count = $role->count_all();
-	    
-	    Message::instance()->set('Total roles '.$role_count);
-	    Breadcrumbs::instance()->add('Management', 'admin/')
-		->add('Roles', 'admin/roles');
-
+	$role = ORM::factory('role');
+	$page = max($this->request->param('page'), 1);
+	$items = 20;
+	$offset = ($items * ($page - 1));
+	$count = $role->count_all();
+	$pagination = Pagination::factory(array(
+					      'current_page' => array(
+						  'source' => 'query_string', 
+						  'key' => 'page'
+						  ),
+					      'total_items' => $role->count_all(),
+					      'items_per_page' => $items,
+					      ));
+	$this->template->roles = $role->order_by('name', 'ASC')
+	    ->limit($pagination->items_per_page)
+	    ->offset($pagination->offset)
+	    ->find_all()->as_array(null, array('id', 'name', 'description'));
+	$this->template->page_links = $pagination->render();
+	$this->template->offset = $pagination->offset;
+	
+	$role_count = $role->count_all();
+	
+	Message::instance()->set('Total roles '.$role_count);
+	Breadcrumbs::instance()->add('Management', 'admin/')
+	    ->add('Roles', 'admin/roles');
+	
     }
-
+    
     public function action_create_role() {
+
+	$post = Validate::factory($_POST);
+	$post->rule('role', 'not_empty')
+	    ->rule('description', 'not_empty')
+	    ->filter(true, 'trim');
+        if(strtolower(Request::$method) === 'post' && $post->check()) {
+            $post = (object)$post->as_array();
+	    $roles = ORM::factory('role')->find_all()->as_array(null, 'name');
+	    
+	    if(!in_array($post->role, $roles)) {
+		$create_role = ORM::factory('role');
+		$create_role->name = $post->role;
+		$create_role->description = $post->description;
+		$create_role->save();
+		
+	    } else {
+		Message::instance()->set('Role already exists.');
+	    }
+	}
+
 	$this->request->redirect("admin/roles/");
     }
-
 
   }
--- www/sites/default/views/admin/roles/list.php
+++ www/sites/default/views/admin/roles/list.php
@@ -1,7 +1,8 @@
 <fieldset>
 <legend>Create a new role:</legend>
 <form name="create-user" method="post" action="admin/roles/create_role">
-<label for="role">Name:</label><input type="text" name="role" />
+<label for="role_name">Name:</label><input type="text" name="role" value="" />
+<label for="role_description">Description:</label><input type="text" name="description" value="" />
 <input type="submit" value="create" name="create-role" />
 </form>
 </fieldset>

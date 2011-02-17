74228f8fa1ca2a9918e15762bc108307888d8874
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-17 19:16:11 +0000

    embed work
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@225 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/assets/scripts/map/embed.js
+++ www/sites/default/assets/scripts/map/embed.js
@@ -20,5 +20,19 @@ $(document).ready(function() {
         Sourcemap.map_instance.map.zoomToExtent(
             Sourcemap.map_instance.getStopLayer(sc.instance_id).getDataExtent()
         );
+        var overlay = $('<div class="map-overlay" id="map-overlay"></div>');
+        overlay.css("width", "100%").css("height", "100%").css("background-color", "red")
+            .css("position", "absolute").css("top", 0).css("left", 0).css("z-index", 5000)
+            .css("overflow", "hidden").hide();
+        Sourcemap.map_overlay = overlay;
+        $(Sourcemap.map_instance.map.div).css("position", "relative");
+        $(Sourcemap.map_instance.map.div).append(overlay);
+        Sourcemap.TPL_PATH = "sites/default/assets/scripts/tpl/";
+        Sourcemap.template('embed/overlay/supplychain', function(p, tx, th) {
+            $(Sourcemap.map_overlay).html(th).show('slide');
+        }, {});
+        $(Sourcemap.map_overlay).click(function() {
+            $(this).hide('slide');
+        });
     });
 });

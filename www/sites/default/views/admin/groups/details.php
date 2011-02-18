571803302b6fef1cc9c5a44ea788aae2adc399dc
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-18 20:51:28 +0000

    HTML::chars()
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@233 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/admin/groups/details.php
+++ www/sites/default/views/admin/groups/details.php
@@ -4,13 +4,13 @@
 <input type="submit" value="Add" />
 </form><br />
 
-<strong>Groupname:</strong> <?=$group_name;?><br />
-<strong>Owner:</strong> <a href="admin/users/<?=$owner['id'];?>"><?=$owner['username']; ?></a><br />
+<strong>Groupname:</strong> <?=HTML::chars($group_name)?><br />
+<strong>Owner:</strong> <a href="admin/users/<?=$owner['id'];?>"><?=HTML::chars($owner['username']) ?></a><br />
 <?php if(!empty($members)): ?>
 <strong>Group Members:</strong> 
 <?php foreach ($members as $member) { ?>
      <form name="delete-member" method="post" action="admin/groups/<?= $group_id?>/delete_member">
-	 <a href="admin/users/<?=$member['id'];?>"><?=$member['username'];?></a>
+     <a href="admin/users/<?=$member['id'];?>"><?=HTML::chars($member['username'])?></a>
      <input type="hidden" name="username" value="<?= $member['username'];?>"><input type="submit" value="delete"/></form> 
  <?php     }?><br />
  <?php endif; ?><br />

e7d3d32d683af909ad5dbbe1e2122da69d2f9ae9
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-22 15:40:49 +0000

    delete supplychain.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@240 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/admin/list/item/supplychains.php
+++ www/sites/default/views/partial/admin/list/item/supplychains.php
@@ -1,6 +1,8 @@
 <td><?= $item->id ?></td>
 <td><a href="admin/supplychains/<?= $item->id ?>"><?= Html::chars($item->created) ?></a></td>
 <td><?= Html::chars($item->owner) ?></td>
+<form name="delete-supplychain" method="post" action="admin/supplychains/<?= $item->id?>/delete_supplychain">
+<td><input type ="submit" value="delete" /></form></td>
 
 
 

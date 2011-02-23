186a08971406a4cc9a1c17c9c77897d631f07ebd
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-23 16:44:45 +0000

    overview (not completed yet!)
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@245 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/admin/dashboard.php
+++ www/sites/default/views/admin/dashboard.php
@@ -1,9 +1,13 @@
 <h2>Management Dashboard</h2>
 <dl>
+<dt><a href="admin/overview">Overview</a></dt>
+<dd>Overview.</dd>
 <dt><a href="admin/supplychains">Supplychains</a></dt>
 <dd>Browse and manage supplychains.</dd>
 <dt><a href="admin/users">Users</a></dt>
 <dd>Manage users.</dd>
 <dt><a href="admin/groups">Groups</a></dt>
 <dd>Manage groups.</dd>
+<dt><a href="admin/roles">Roles</a></dt>
+<dd>Manage Roles.</dd>
 </dl>
---
+++ www/sites/default/views/admin/overview.php
@@ -0,0 +1,3 @@
+Updates since last week: <strong><?= $supplychain_lastweek?></strong> supplychains have been created and <strong><?= $user_lastweek?></strong> users have logged in since last week.<br />
+The administrator last login was on <strong><?= $admin_lastlogin?></strong><br />
+

6633de5db4df191ef0b6ccf273c709cf8d048a73
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-23 16:44:45 +0000

    overview (not completed yet!)
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@245 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/sites/default/views/partial/admin/list/item/dashboard.php
@@ -0,0 +1,9 @@
+<td><?= $item->id ?></td>
+<td><?= Html::chars($item->created) ?></td>
+<td><div class="map-thumb" style="width: 10%; float: left;">
+<a href="map/view/<?= $item->id ?>"><img style="width: 80%;" src="<?=$item->static ?>" /></a>
+</div>
+
+
+
+

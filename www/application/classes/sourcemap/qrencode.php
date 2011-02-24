497ef31a3c927a9a201a6586f4a51b61c00bb41c
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-24 15:18:08 +0000

    qr code sizing
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@248 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/classes/sourcemap/qrencode.php
+++ www/application/classes/sourcemap/qrencode.php
@@ -1,6 +1,8 @@
 <?php
 class Sourcemap_Qrencode {
-   public static function encode($str) {
-        return QRcode::png($str);
+   public static function encode($str, $sz=3) {
+        $sz = (int)$sz;
+        $sz = min(max(1, $sz), 10);
+        return QRcode::png($str, false, QR_ECLEVEL_L, $sz);
    }
 }

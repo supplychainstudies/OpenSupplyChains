37b3895f07462e87a82ca8e4df80097621225cab
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-25 03:52:15 +0000

    huge data drop - ip loc
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@250 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ db/schema/05.iploc.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE iploc_block (
+    st bigint,
+    en bigint,
+    city character varying(64),
+    region character varying(64),
+    country character varying(64),
+    postal_code character varying(32),
+    latitude double precision,
+    longitude double precision
+);
+
+
--- www/sites/default/classes/controller/welcome.php
+++ www/sites/default/classes/controller/welcome.php
@@ -5,6 +5,12 @@ class Controller_Welcome extends Sourcemap_Controller_Layout {
     public $template = 'welcome';
 
     public function action_index() {
+        die(
+            print_r(
+                Model::factory('iploc')->find_blocks(0),
+                true
+            )
+        );
         $this->layout->page_title = 'Welcome to Sourcemap.';
         $this->template->supplychains = ORM::factory('supplychain')
             ->where(DB::expr('other_perms & '.Sourcemap::READ), '>', 0)

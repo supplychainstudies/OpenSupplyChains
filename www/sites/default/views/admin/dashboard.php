44d3240efa34cef629c1c34a1302035d28eab8e0
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-25 20:30:37 +0000

    overview appears on the management dashboard.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@258 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/admin/dashboard.php
+++ www/sites/default/views/admin/dashboard.php
@@ -1,7 +1,13 @@
 <h2>Management Dashboard</h2>
+
+<strong>Updates since last week:</strong> <strong><?= $supplychain_lastweek?></strong> supplychains have been created, <strong><?= $user_lastweek?></strong> users have been created  and <strong><?= $user_lastlogin?></strong> users have logged in since last week.<br />
+The administrator last login was on <strong><?= $admin_lastlogin?></strong><br /><br />
+The maximum number of stops entered so far are <strong><?= $stop?></strong> (<a href="map/view/<?= $supplychain_id?>">view map</a>)<br />
+The maximum number of hops entered so far are <strong><?= $hop?></strong> (<a href="map/view/<?= $supplychain_hop_id?>">view map</a>)<br /><br />
+
+<strong>Today's Updates:</strong> <strong><?= $user_today?></strong> users have been created today and <strong><?= $supplychain_today?></strong> supplychains have been created today.
+
 <dl>
-<dt><a href="admin/overview">Overview</a></dt>
-<dd>Overview.</dd>
 <dt><a href="admin/supplychains">Supplychains</a></dt>
 <dd>Browse and manage supplychains.</dd>
 <dt><a href="admin/users">Users</a></dt>

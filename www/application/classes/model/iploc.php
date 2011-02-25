7a32ceabb442bf73506668b356b779a7ef2462d6
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-02-25 14:48:16 +0000

    ip loc
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@251 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/application/classes/model/iploc.php
@@ -0,0 +1,15 @@
+<?php
+class Model_Iploc extends Model {
+    public function find($ipaddr) {
+        $decip = Sourcemap_Ip::dot2dec($ipaddr);
+        $loc = false;
+        if($decip) {
+            $sql = sprintf(
+                'select * from iploc_block where st <= %d and en >= %d', 
+                $decip, $decip
+            );
+            $loc = $this->_db->query(Database::SELECT, $sql, true);
+        }
+        return $loc;
+    }
+}

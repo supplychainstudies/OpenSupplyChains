46f5049f2dad0f0602f610fda5c0f09c9f6a36c7
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-03 01:19:26 +0000

    style tweak
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@284 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/stonyfield/views/welcome.php
+++ www/sites/stonyfield/views/welcome.php
@@ -5,6 +5,6 @@
     <li class="other"><a href="other">Other Ingredients</a></li>
 </ul>
 <div id="map">
-<iframe height="500" width="90%" scrolling="no" frameborder="0" 
+<iframe height="500" width="100%" scrolling="no" frameborder="0" 
     src="http://alpha.sourcemap.org/map/embed/<?= $scid ?>?tour=yes&amp;tour_start_delay=0&amp;tour_interval=3"></iframe>
 </div>

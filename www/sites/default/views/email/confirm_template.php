346828962946e1f900f13b3781993babb1211945
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-03 21:43:44 +0000

    sends email after creating an account. validation not working yet, some more work needs to be done for confirmation. Not done yet!
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@298 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/sites/default/views/email/confirm_template.php
@@ -0,0 +1,3 @@
+<p>Dear <?=HTML::chars($email_vars['username']); ?>,</p>
+<p>Thank you for creating a Sourcemap account with us. Please click on the below link to confim your email address:</p>
+<p>-Sourcemap Team</p>

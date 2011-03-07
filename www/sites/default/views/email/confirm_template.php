9a925c7b3308e516dfbfbdc7c291b88696e78d37
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-07 15:49:11 +0000

    User registration works on alpha since the link the view points to alpha.sourcemap.org
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@310 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/email/confirm_template.php
+++ www/sites/default/views/email/confirm_template.php
@@ -1,3 +1,4 @@
 <p>Dear <?=HTML::chars($email_vars['username']); ?>,</p>
-<p>Thank you for creating a Sourcemap account with us. Please click on the below link to confim your email address:</p>
+<p>Thank you for creating a Sourcemap account with us. Please click on the below link to confim your email address:<br />
+     http://alpha.sourcemap.org/register/confirm/<?=$email_vars['hash_value']?></p>
 <p>-Sourcemap Team</p>

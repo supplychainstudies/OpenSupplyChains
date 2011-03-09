09ff70071007f1544cc36852f31bf1907597addc
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-09 16:14:49 +0000

    session config and small reorganization
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@320 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/sites/default/views/email/confirm.php
@@ -0,0 +1,6 @@
+<p>Dear <?=HTML::chars($email_vars['username']); ?>,</p>
+<p>Thank you for creating a Sourcemap account with us. 
+Please click on the below link to confim your email address:<br />
+     <?= HTML::anchor("register/confirm?u=<?={$email_vars['hash_value']}") ?>
+</p>
+<p>- The Sourcemap Team</p>
--- www/sites/default/views/email/confirm_template.php
+++
@@ -1,4 +0,0 @@
-<p>Dear <?=HTML::chars($email_vars['username']); ?>,</p>
-<p>Thank you for creating a Sourcemap account with us. Please click on the below link to confim your email address:<br />
-     http://localhost/smap/trunk/www/register/confirm?u=<?=$email_vars['hash_value']?></p>
-<p>-Sourcemap Team</p>

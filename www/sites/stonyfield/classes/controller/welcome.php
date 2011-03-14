9bc217f5be7f2996f9358da94ce98dff31edc1a9
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-14 09:55:53 +0000

    attempt to fix dialog problem
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@342 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/stonyfield/classes/controller/welcome.php
+++ www/sites/stonyfield/classes/controller/welcome.php
@@ -8,11 +8,11 @@ class Controller_Welcome extends Sourcemap_Controller_Layout {
         $this->layout->styles = array(
             'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.7/themes/base/jquery-ui.css'
         );
-        $this->layout->scripts = array(
+        /*$this->layout->scripts = array(
             'sourcemap-jquery', 
             'stonyfield-embed', 
             'sourcemap-map'
-        );
+        );*/
         $alias = ORM::factory('supplychain_alias')
             ->where('site', '=', 'stonyfield')
             ->where('alias', '=', $scalias)

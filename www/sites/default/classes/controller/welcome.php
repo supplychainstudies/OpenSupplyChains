cd957f83af97282674399c9c31d591dacd5707cc
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-17 16:35:44 +0000

    style links
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@382 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/classes/controller/welcome.php
+++ www/sites/default/classes/controller/welcome.php
@@ -5,15 +5,6 @@ class Controller_Welcome extends Sourcemap_Controller_Layout {
     public $template = 'welcome';
 
     public function action_index() {
-        /*header('Content-Type: text/plain');
-        $ip = '18.85.2.115';
-        print "$ip\n";
-        die(
-            print_r(
-                Sourcemap_Ip::find_ip($ip),
-                true
-            )
-        );*/
         $this->layout->page_title = 'Welcome to Sourcemap.';
         $this->template->supplychains = ORM::factory('supplychain')
             ->where(DB::expr('other_perms & '.Sourcemap::READ), '>', 0)
@@ -24,7 +15,7 @@ class Controller_Welcome extends Sourcemap_Controller_Layout {
         );
         $this->layout->styles = array(
             'sites/default/assets/styles/reset.css', 
-            'sites/default/assets/styles/sourcemap.less?v=2'
+            'sites/default/assets/styles/general.less?v=2'
         );
     }
 }

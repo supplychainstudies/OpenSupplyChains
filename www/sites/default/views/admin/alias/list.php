977e096d09d62b64cf22a646bc1db89173c425b9
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-17 17:17:20 +0000

    create alias along with the listing. Small fix in supplychain create alias function.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@383 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/admin/alias/list.php
+++ www/sites/default/views/admin/alias/list.php
@@ -1,4 +1,15 @@
+<form name="alias-info" method="post" action="">
+<label for="site">Site:</label>
+<input type="text" name="site" class="input text" value=""/>
+
+<label for="alias">Alias:</label>
+<input type="text" name="alias" class="input text" value=""/>
+
+<label for="supplychain_id">Supplychain_id:</label>
+<input type="text" name="supplychain_id" class="input text" value=""/>
+
+<input type="submit" value="Create Alias" />
+</form><br />
+  
 <?= View::factory('partial/admin/list', array('list' => $supplychain_alias, 'list_type' => 'alias')) ?>
 <?php echo $page_links; ?>
-
-    

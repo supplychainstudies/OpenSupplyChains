cb13f1e6e6c08d0a162d67b7b0f3eae6629f2f58
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-22 13:56:14 +0000

    * Removed some inline styles * Trying out background loader gif.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@396 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/layout/embed.php
+++ www/sites/default/views/layout/embed.php
@@ -24,7 +24,7 @@
 
 </head>
 
-<body id="supplychain" style="width: 100%;height: 100%;margin: 0; padding: 0;">
+<body id="embedded-supplychain">
 <?= $content ?>
 
     <?= isset($scripts) ? Sourcemap_JS::script_tags($scripts) : '' ?>
--- www/sites/default/views/map/embed.php
+++ www/sites/default/views/map/embed.php
@@ -1,1 +1,1 @@
-<div id="sourcemap-map-embed" style="width: 100%; height: 100%; background-color: #ddd; margin: 0; padding: 0;"></div>
+<div id="sourcemap-map-embed"></div>

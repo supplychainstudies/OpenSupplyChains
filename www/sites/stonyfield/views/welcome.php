77889be65b64e09b89ba16cdd52f5810027f2576
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-23 14:33:37 +0000

    removed banner on sf embed
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@415 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/stonyfield/views/welcome.php
+++ www/sites/stonyfield/views/welcome.php
@@ -17,5 +17,5 @@
 </ul>
 <div id="map">
 <iframe height="500" width="100%" scrolling="no" frameborder="0" 
-    src="http://alpha.sourcemap.org/map/embed/<?= $scid ?>?tour=yes&amp;tour_start_delay=0&amp;tour_interval=3"></iframe>
+    src="http://alpha.sourcemap.org/map/embed/<?= $scid ?>?tour=yes&amp;tour_start_delay=0&amp;tour_interval=3&amp;banner=no"></iframe>
 </div>

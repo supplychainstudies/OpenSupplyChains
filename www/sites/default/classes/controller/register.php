4ab0f80472f0aa26489411e82e186be30acdb773
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-23 21:05:26 +0000

    email class reorg and tweaks
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@433 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/classes/controller/register.php
+++ www/sites/default/classes/controller/register.php
@@ -44,29 +44,29 @@ class Controller_Register extends Sourcemap_Controller_Layout {
             
             if($post->password == $post->confirm_password) {
                 $create = ORM::factory('user');
-		$create->username = $post->username;                
-		$create->email = $post->email;
-		$create->password = $post->password;
+        $create->username = $post->username;                
+        $create->email = $post->email;
+        $create->password = $post->password;
 
-		try {
-		    $create->save();  
-		    $created = ORM::factory('user', $create->id)->created;
-		    $hash_value = Auth::instance()->hash($post->username.$post->email.$created);
-		    if(isset($post->identifier) && !empty($post->identifier)) {
-			$openidusers = ORM::factory('openidusers');
-			$openidusers->identifier = $identifier;
-			$openidusers->user_id = $create->id;
-			$openidusers->save();
-		    }
-		    $this->email_user($post->username, $post->email, $hash_value);	    
-		} catch (Exception $e){
-		    Message::instance()->set('Could not register the user.');
-		}
-		
-	    } else {
-		Message::instance()->set('Passwords did not match.');
-	    }
-	}
+        try {
+            $create->save();  
+            $created = ORM::factory('user', $create->id)->created;
+            $hash_value = Auth::instance()->hash($post->username.$post->email.$created);
+            if(isset($post->identifier) && !empty($post->identifier)) {
+            $openidusers = ORM::factory('openidusers');
+            $openidusers->identifier = $identifier;
+            $openidusers->user_id = $create->id;
+            $openidusers->save();
+            }
+            $this->email_user($post->username, $post->email, $hash_value);        
+        } catch (Exception $e){
+            Message::instance()->set('Could not register the user.');
+        }
+        
+        } else {
+        Message::instance()->set('Passwords did not match.');
+        }
+    }
     }
     
     
@@ -80,111 +80,111 @@ class Controller_Register extends Sourcemap_Controller_Layout {
         $subject = 'Email confirmation for Sourcemap account';
         $body = View::factory('email/confirm')->bind('email_vars', $email_vars);
         
-	try {	    
-	    Email_Template::send_email($to, $subject, $body);
-	} catch (Exception $e) {
-	    Message::instance()->set('Sorry, could not send an email.');
-	}
+    try {        
+        Sourcemap_Email_Template::send_email($to, $subject, $body);
+    } catch (Exception $e) {
+        Message::instance()->set('Sorry, could not send an email.');
+    }
     }
 
     public function action_confirm(){
     
-	$hash = $_GET['u'];
+    $hash = $_GET['u'];
         $users = ORM::factory('user')->find_all()->as_array('id', array('id','username', 'email', 'created'));
         foreach($users as $user) {
             if (Auth::instance()->hash($user->username.$user->email.$user->created) == $hash) {
-		$user_confirm = ORM::factory('user', $user->id);
-		$user_confirm->flags =2;
+        $user_confirm = ORM::factory('user', $user->id);
+        $user_confirm->flags =2;
 
-		try {
-		    $user_confirm->save();    
-		    //add a default login role when a new user is created
-		    $role = ORM::factory('role', array('name' => 'login'));
-		    $user_confirm->add('roles', $role)->save();
-		    Message::instance()->set('Thank you, your registration is now complete.');
-		} catch (Exception $e) {
-		    Message::instance()->set('Could not complete the registration.');
-		}
-		
-	    }
-	}
+        try {
+            $user_confirm->save();    
+            //add a default login role when a new user is created
+            $role = ORM::factory('role', array('name' => 'login'));
+            $user_confirm->add('roles', $role)->save();
+            Message::instance()->set('Thank you, your registration is now complete.');
+        } catch (Exception $e) {
+            Message::instance()->set('Could not complete the registration.');
+        }
+        
+        }
+    }
     }
 
     
     public function action_loginopenid() {
-	
-	if(isset($_POST['token'])) {
-	    $token = $_POST['token'];
-	 
-	    $apikey = '0cb119b5123b1731a13c0979937af366504944a4';
-	    $post_data = array('token' => $_POST['token'],
-			       'apiKey'=> $apikey,
-			       'format' => 'json');
+    
+    if(isset($_POST['token'])) {
+        $token = $_POST['token'];
+     
+        $apikey = '0cb119b5123b1731a13c0979937af366504944a4';
+        $post_data = array('token' => $_POST['token'],
+                   'apiKey'=> $apikey,
+                   'format' => 'json');
 
-	    $curl = curl_init();
-	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($curl, CURLOPT_URL, 'https://rpxnow.com/api/v2/auth_info');
-	    curl_setopt($curl, CURLOPT_POST, true);
-	    curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
-	    curl_setopt($curl, CURLOPT_HEADER, false);
-	    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-	    $raw_json = curl_exec($curl);
-	    curl_close($curl); 	    
-	    
-	    $auth_info = json_decode($raw_json, true);	    
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_URL, 'https://rpxnow.com/api/v2/auth_info');
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $raw_json = curl_exec($curl);
+        curl_close($curl);         
+        
+        $auth_info = json_decode($raw_json, true);        
 
-	    if ($auth_info['stat'] == 'ok') {
-		$profile = $auth_info['profile'];
-		if (isset($profile['identifier'])) {
-		    $identifier = $profile['identifier'];
-		}
-		if (isset($profile['name'])) {
-		    $name = $profile['name'];
-		    if (isset($name['givenName'])) {
-			$username = $name['givenName'];
-		    } else {
-			$username = $profile['displayName'];
-		    }
-		}	 
-		
-		if (isset($profile['verifiedEmail'])) {
-		    $email = $profile['verifiedEmail'];
-		}
-		
-		$user = ORM::factory('user');
-		$all_users = $user->find_all()->as_array(null, 'username');
+        if ($auth_info['stat'] == 'ok') {
+        $profile = $auth_info['profile'];
+        if (isset($profile['identifier'])) {
+            $identifier = $profile['identifier'];
+        }
+        if (isset($profile['name'])) {
+            $name = $profile['name'];
+            if (isset($name['givenName'])) {
+            $username = $name['givenName'];
+            } else {
+            $username = $profile['displayName'];
+            }
+        }     
+        
+        if (isset($profile['verifiedEmail'])) {
+            $email = $profile['verifiedEmail'];
+        }
+        
+        $user = ORM::factory('user');
+        $all_users = $user->find_all()->as_array(null, 'username');
                 $all_emails = $user->find_all()->as_array(null, 'email');
-		$auto_password = text::random($type = 'alnum', $length = 6);  
-		
-			
-		if(!in_array($email, $all_emails)){
-		    $username = $this->get_username($username, $all_users);
-		    
-		} else {
-		    $get_user = $user->where('email', '=', $email)->find();
-		    $username = $get_user->username;
-		    
-		}
+        $auto_password = text::random($type = 'alnum', $length = 6);  
+        
+            
+        if(!in_array($email, $all_emails)){
+            $username = $this->get_username($username, $all_users);
+            
+        } else {
+            $get_user = $user->where('email', '=', $email)->find();
+            $username = $get_user->username;
+            
+        }
 
-	    }
-	}
-	$this->template->email = $email;
-	$this->template->username = $username;
-	$this->template->password = $auto_password;
-	$this->template->identifier = $identifier;
-	
+        }
+    }
+    $this->template->email = $email;
+    $this->template->username = $username;
+    $this->template->password = $auto_password;
+    $this->template->identifier = $identifier;
+    
     }
 
 
     public function get_username($username, $all_users){
-	$count =0;
-	$test = in_array($username, $all_users);
-	while($test) {
-	    $count++;
-	    $test = in_array($username."-".$count, $all_users);
-	    $username = $username."-".$count;
-	}
-	return $username;
+    $count =0;
+    $test = in_array($username, $all_users);
+    while($test) {
+        $count++;
+        $test = in_array($username."-".$count, $all_users);
+        $username = $username."-".$count;
+    }
+    return $username;
     }
 
 

3045df2fe81859d3c07db3cdd6f3e57e2fa1651a
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-23 21:05:26 +0000

    email class reorg and tweaks
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@433 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/classes/sourcemap/email/template.php
+++ www/application/classes/sourcemap/email/template.php
@@ -1,15 +1,13 @@
 <?
-class Email_Template {
+class Sourcemap_Email_Template {
     
     public static function send_email($to, $subject, $body) {
-	
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $headers .= 'From: Sourcemap Team <smita@sourcemap.org>' . "\r\n";
-	
-	mail($to, $subject, $body, $headers);
+    
+        mail($to, $subject, $body, $headers);
     }
 
 }
-
-?>

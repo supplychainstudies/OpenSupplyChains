92c8953e626d396b58c88ea635516c8a7decc2e9
smita <smita@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-03-23 18:11:03 +0000

    embed instead of overlay for social sign in
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@426 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/register.php
+++ www/sites/default/views/register.php
@@ -1,12 +1,16 @@
 <div class="register-form" style="float: left; width: 5.2em">
 <fieldset>
-<form name="register" method="post" action="">
+<form name="register" method="post" action="register">
      <label for="username">Username:</label><input type="text" name="username" <?php if(isset($username)){?> value="<?=$username?>" <?}?> /><br />
      <label for="email">Email:</label><input type="text" name="email"  <?php if(isset($email)) {?> value="<?=$email?>" <?}?> /><br /> 
      <label for="password">Password:</label><input name="password" <?php if(isset($password)) {?> value="<?=$password?>" type="text" <?} else { ?>type="password"<?}?>/><br />
-     <label for="confirm_password">Confirm:</label>     <input name="confirm_password" <?php if(isset($password)) {?> value="<?=$password?>" type="text" <?} else { ?>type="password"<?}?>/><br />
-<input type="submit" value="Register" name="register" />
+     <label for="confirm_password">Confirm:</label><input name="confirm_password" <?php if(isset($password)) {?> value="<?=$password?>" type="text" <?} else { ?>type="password"<?}?>/><br />
+     <?if (isset($identifier)) {?><input type="hidden" name="identifier" value="<?=$identifier?>"/><br /><?}?>
+<input type="submit" value="Register" name="register" /></form>
+<br />
 
 </div>
+     <div class="social-signin" style="float: right";>     
+<iframe src="http://sourcemap1.rpxnow.com/openid/embed?token_url=http%3A%2F%2Falpha.sourcemap.org%2Fregister%2Floginopenid"  scrolling="no"  frameBorder="no"  allowtransparency="true"  style="width:400px;height:240px"></iframe>  
 
-
+</div>

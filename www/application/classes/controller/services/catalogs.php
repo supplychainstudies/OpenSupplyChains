c08a4174298f1d014a83ee5a9bdad66e275a1c56
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-04-11 17:54:29 +0000

    error handling for catalogs service
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@499 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/classes/controller/services/catalogs.php
+++ www/application/classes/controller/services/catalogs.php
@@ -1,7 +1,11 @@
 <?php
 class Controller_Services_Catalogs extends Sourcemap_Controller_Service {
-    public function action_get($nm) {
-        $results = Sourcemap_Catalog::get('osi', array('name' => 'ORANGES'));
+    public function action_get($catnm) {
+        try {
+            $results = Sourcemap_Catalog::get($catnm, $_GET);
+        } catch(Exception $e) {
+            return $this->_internal_server_error('Catalog is broken: '.$e);
+        }
         $this->response = $results;
     }
 }

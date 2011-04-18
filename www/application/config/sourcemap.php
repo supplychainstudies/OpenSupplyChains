57db4903cc899faff83e3c159e9df21015d36c0e
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-04-18 18:08:09 +0000

    removed configs.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@512 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/config/sourcemap.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-return array(
-    'base_url' => '',
-    'cache_dir' => SOURCEMAP_CACHE_PATH,
-    'upload_dir' => '/tmp/',
-    'debug' => false,
-    'site' => SOURCEMAP_SITE
-);

a6c40dfc307596adcd3901ef8143050bc0b15299
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-04-19 19:03:57 +0000

    moved sessions to db. this will break everything for everybody. unless you run db-migrate dev up 08
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@521 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/classes/sourcemap.php
+++ www/application/classes/sourcemap.php
@@ -21,11 +21,6 @@ class Sourcemap {
     public static function init() {
         if(isset(Kohana::$environment))
             self::$env = Kohana::$environment;
-        session_set_save_handler(
-            array('Sourcemap', "_sess_open"), array('Sourcemap', "_sess_close"), 
-            array('Sourcemap', "_sess_read"), array('Sourcemap', "_sess_write"), 
-            array('Sourcemap', "_sess_destroy"), array('Sourcemap', "_sess_gc")
-        );
         Sourcemap_JS::add_packages(
             isset(Kohana::config('js')->packages) ? 
                 Kohana::config('js')->packages : array()
@@ -34,52 +29,8 @@ class Sourcemap {
         Sourcemap_JS::$minified = self::$env == self::DEV ? false : true;
         Sourcemap_JS::$bundle_path = 'assets/scripts/bundles/'.self::site().'/';
         Sourcemap_CSS::$convert_less = self::$env == self::DEV ? false : true;
-    }
-
-    public static function _sess_open($save_path, $session_name) {
-        $sess_save_path = self::$_sess_save_path = $save_path ? $save_path : self::$session_dir;
-        return(true);
-    }
-
-    public static function _sess_close() {
-        return(true);
-    }
-
-    public static function _sess_read($id) {
-        $sess_save_path = self::$_sess_save_path;
-
-        $sess_file = "$sess_save_path/sess_$id";
-        return (string) @file_get_contents($sess_file);
-    }
-
-    public static function _sess_write($id, $sess_data) {
-        $sess_save_path = self::$_sess_save_path;
-
-        $sess_file = "$sess_save_path/sess_$id";
-        if ($fp = fopen($sess_file, "w")) {
-            $return = fwrite($fp, $sess_data);
-            fclose($fp);
-            return $return;
-        } else {
-            return(false);
-        }
-
-    }
-
-    public static function _sess_destroy($id) {
-        $sess_save_path = self::$_sess_save_path;
-        $sess_file = "$sess_save_path/sess_$id";
-        return(unlink($sess_file));
-    }
-
-    public static function _sess_gc($maxlifetime) {
-        $sess_save_path = self::$_sess_save_path;
-        foreach (glob("$sess_save_path/sess_*") as $filename) {
-            if (filemtime($filename) + $maxlifetime < time()) {
-                unlink($filename);
-            }
-        }
-        return true;
+        // Use db for session storage
+        Session::$default = 'database';
     }
 
     public static function environment() {

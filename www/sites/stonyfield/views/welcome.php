dd9ad5c5d4ff3138b7488add7ab3cd4de6993874
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-04-19 14:20:48 +0000

    sf tweaks
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@518 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/stonyfield/views/welcome.php
+++ www/sites/stonyfield/views/welcome.php
@@ -1,7 +1,6 @@
 <ul class="map-nav<?= isset($scalias) ? " $scalias" : '' ?>">
-    <li class="yogurt"><a href="yogurt">Yogurt</a></li>
-    <li class="sweeteners"><a href="sweeteners">Fruit and Sweeteners</a></li>
-    <li class="dairy"><a href="#">Dairy</a>
+    <li class="yogurt"><a href="yogurt">Everything</a></li>
+    <li class="dairy"><a href="#">Milk</a>
         <ul>
             <li><a href="northeast">Northeast</a></li>
             <li><a href="southeast">Southeast</a></li>
@@ -13,6 +12,7 @@
             <li><a href="california">California</a></li>
         </ul>
     </li>
+    <li class="sweeteners"><a href="sweeteners">Fruit and Sweeteners</a></li>
     <li class="other"><a href="other">Other Ingredients</a></li>
 </ul>
 <div id="map">

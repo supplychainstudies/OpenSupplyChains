e39e4b46d4735878909831834ec7febdf47274c6
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-04-28 17:22:37 +0000

    forgot to add a few files for 545
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@548 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/assets/scripts/sourcemap/supplychain/create.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    // Set text inputs to inactive, create status & error boxes
+    $('.form input[type="text"]')
+        .addClass("inactive")
+        .click(function(){
+            $(this).removeClass("inactive");
+        })
+        .after("<div class='error' style='display:none'></div>");
+    
+    // Add asterisk to required fields
+    $('.form input.required')
+        .prev("label")
+        .append('<span class="highlighted">*</span>')
+        .next()
+        .after("<div class='status inactive'></div>");
+
+    // Disable submit until required fields are filled in 
+    $(".form input[type='submit']").attr("disabled","disabled");
+
+    var validated = function(){
+        if ($('.status.inactive').length > 0){
+            return false;
+        }
+        else {
+            return true;
+        }
+    };
+    
+    // Enable submit on validation
+    $(".form input.required").bind('keyup change', function(){
+        if ($(this).val().length > 0){
+            $(this).next().removeClass("inactive");    
+        }
+        else {
+            $(this).next().addClass("inactive");
+        }
+        if (validated()){
+            $(".form input[type='submit']").removeAttr("disabled");
+        }
+        else{
+            $(".form input[type='submit']").attr("disabled","disabled");
+        }
+});
+    
+
+});
+
+$('.form fieldset form').submit(function(e) {
+    $(".form .error").hide();
+ 
+    // Create new supplychain object
+    var sc = new Sourcemap.Supplychain();
+    sc.attributes.title = $("#title").val();
+    sc.attributes.description = $("#desc").val();
+    sc.attributes.tags = $("#tags").val();
+    
+    // Save the supplychain and retrieve its url using a custom callback
+    var o = {};
+    o.success = function(sc, scid, uri){
+        document.location.href = "map/view/" + scid;
+    } 
+    Sourcemap.saveSupplychain(sc,o);
+
+    return false;
+});
+
---
+++ www/sites/default/classes/controller/create.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Description Create a Map page
+ * @package    Sourcemap
+ * @author     Alex Ose 
+ * @copyright  (c) Sourcemap
+ * @license    http://blog.sourcemap.org/terms-of-service
+ */
+
+class Controller_Create extends Sourcemap_Controller_Layout {
+
+    public $layout = 'layout';
+    public $template = 'create';
+    
+    public function action_index() {
+        if(!Auth::instance()->get_user())
+            return $this->_forbidden('You must be logged in to create a map.');
+
+        $this->layout->scripts = array(
+            'sourcemap-core', 'sourcemap-template', 'sourcemap-create' 
+        );
+        $this->layout->styles = array(
+            'assets/styles/style.css', 
+            'assets/styles/sourcemap.less?v=2'
+        );
+    }
+}
+  
+
+

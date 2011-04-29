08659edf0974049f4329128f6c44a0a0b5f592fa
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-04-29 08:41:10 +0000

    alias delete fix
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@553 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/admin/list/item/alias.php
+++ www/sites/default/views/partial/admin/list/item/alias.php
@@ -2,6 +2,6 @@
 <td><?= Html::chars($item->site) ?></a></td>
 <td><?= Html::chars($item->alias) ?></td>
 <td><a href="admin/supplychains/<?= $item->id ?>"><?= $item->supplychain_id?></td>
-<form name="delete-alias" method="post" action="admin/alias/<?= $item->id?>/delete_supplychain_alias">
+<form name="delete-alias" method="post" action="admin/aliases/<?= $item->id?>/delete_supplychain_alias">
 <td><input type ="submit" value="delete" /></form></td>
 

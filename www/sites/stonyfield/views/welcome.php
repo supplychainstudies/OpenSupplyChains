b8bdefd4a0dba0f472f8c3284c4b0f0d29892532
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-04 17:12:00 +0000

    stonyfield: removed legend
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@572 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/stonyfield/views/welcome.php
+++ www/sites/stonyfield/views/welcome.php
@@ -19,4 +19,3 @@
 <iframe height="500" width="100%" scrolling="no" frameborder="0" 
     src="http://alpha.sourcemap.org/map/embed/<?= $scid ?>?tour=yes&amp;tour_start_delay=0&amp;tour_interval=3&amp;banner=no"></iframe>
 </div>
-<img src="sites/stonyfield/assets/images/legend.png" />

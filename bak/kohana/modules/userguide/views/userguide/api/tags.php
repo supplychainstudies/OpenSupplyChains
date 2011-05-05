69a764c4a3c05cad23307dd57d1ff335f35197c9
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-05 14:59:34 +0000

    removed bak files
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@577 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- bak/kohana/modules/userguide/views/userguide/api/tags.php
+++
@@ -1,8 +0,0 @@
-<dl class="tags">
-<?php foreach ($tags as $name => $set): ?>
-<dt><?php echo $name ?></dt>
-<?php foreach ($set as $tag): ?>
-<dd><?php echo $tag ?></dd>
-<?php endforeach ?>
-<?php endforeach ?>
-</dl>

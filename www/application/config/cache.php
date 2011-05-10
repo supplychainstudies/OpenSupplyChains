c26377ed23917ec0da0160dfe749acc3ec0f6d2b
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-10 18:07:22 +0000

    featured map admin and better supplychains list service
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/branches/teardown@597 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/config/cache.php
+++ www/application/config/cache.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 return array (
-	'file'    => array(
+	/*'file'    => array(
 		'driver'             => 'file',
 		'cache_dir'          => Kohana::$cache_dir,
 		'default_expire'     => 3600,
@@ -9,5 +9,18 @@ return array (
 		'driver'             => 'file',
 		'cache_dir'          => APPPATH.'cache',
 		'default_expire'     => 3600,
-	)
+	)*/
+    'default' => array(
+        'driver' => 'smemcache',
+        'compression' => false,
+        'servers' => array(
+            array(
+                'host' => 'localhost',
+                'port' => 11234,
+                'persistent' => false
+            )
+        ),
+        'default_expire' => 60,
+        'requests' => 100
+    )
 );

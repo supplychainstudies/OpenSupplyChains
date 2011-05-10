901ceac7762fe004a6495ef3708c87baf8337ec3
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-10 15:47:17 +0000

    teardown: added grid framework, improvements to views
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/branches/teardown@594 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/branding.php
+++ www/sites/default/views/partial/branding.php
@@ -4,57 +4,65 @@
             <h1>Sourcemap</h1>
         </header>
         </a>
-        <nav id="main-navigation">
-            <ul class="nav">
-                <?php if($current_user = Auth::instance()->get_user()): ?>
-                <li class="register-link"><span class="username"><?= HTML::chars($current_user->username) ?>&nbsp;|&nbsp;<a href="auth/logout">Log out</a></li>
-                <?php else: ?>
-                <li class="register-link"><a href="register">Join us</a> or <a href="auth">Log in</a></li>
-                <?php endif; ?>
-                <li id="browse-navigation">
-                    <h3>Browse</h3>            
-                    <ul>
-                        <li><a href="">Sourcemaps</a></li>
-                        <li><a href="">Tools</a>
-                            <ul>
-                                <li><a href="tools/import/csv">Importer</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="">Carbon Catalogue</a>
-                            <ul>
-                                <li><a href="">Part Catalogue</a></li>
-                                <li><a href="">Transport Cat.</a></li>
-                                <li><a href="">Power Cat.</a></li>
-                                <li><a href="">Process Cat.</a></li>
-                                <li><a href="">Endoflife Cat.</a></li>                                                                     
-                            </ul>
-                        </li>
-                        <li><a href="">Members</a></li>      
-                        <li><a href="">Groups</a></li>                    
-                                      
-                    </ul>
-                </li>
-                <li id="info-navigation">
-                    <h3>Info</h3>
-                    <ul>
-                        <li><a href="">About Us</a></li>
-                        <li><a href="">Who We Are</a></li>
-                        <li><a href="">Join Us!</a></li>
-                        <li><a href="">API and Code</a></li>
-                        <li>
-                            <a href="">Help</a>
-                            <ul>
-                                <li><a href="">Data References</a></li>
-                                <li><a href="">FAQs</a></li>                                    
-                            </ul>                                
-                        </li>
-                        <li><a href="">Get In Touch</a></li>                    
-                    </ul>
-                </li>
-            </ul>
-            <div id="search"><input id="search-field" type="search" results="0" placeholder="Search" /></div>
-        </nav>
+        <div id="search">
+            <input id="search-field" type="search" results="0" placeholder="Search" />
+        </div>
+        <ul class="nav">
+            <li id="browse-navigation">
+                <h3>Browse</h3>            
+                <ul>
+                    <li><a href="">Sourcemaps</a></li>
+                    <li><a href="">Tools</a>
+                        <ul>
+                            <li><a href="tools/import/csv">Importer</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Carbon Catalogue</a>
+                        <ul>
+                            <li><a href="">Part Catalogue</a></li>
+                            <li><a href="">Transport Cat.</a></li>
+                            <li><a href="">Power Cat.</a></li>
+                            <li><a href="">Process Cat.</a></li>
+                            <li><a href="">Endoflife Cat.</a></li>                                                                     
+                        </ul>
+                    </li>
+                    <li><a href="">Members</a></li>      
+                    <li><a href="">Groups</a></li>                    
+                </ul>
+            </li>
+            <li id="info-navigation">
+                <h3>Info</h3>
+                <ul>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Who We Are</a></li>
+                    <li><a href="">Join Us!</a></li>
+                    <li><a href="">API and Code</a></li>
+                    <li>
+                        <a href="">Help</a>
+                        <ul>
+                            <li><a href="">Data References</a></li>
+                            <li><a href="">FAQs</a></li>                                    
+                        </ul>                                
+                    </li>
+                    <li><a href="">Get In Touch</a></li>                    
+                </ul>
+            </li>
+            <li id="account-navigation">
+                <h3>Account</h3>
+                <ul>
+                    <?php if($current_user = Auth::instance()->get_user()): // This happens if the user is logged in ?>
+                    <li class="register-link">
+                        <span class="username"><?= HTML::chars($current_user->username) ?>&nbsp;|&nbsp;<a href="auth/logout">Log out</a></span>
+                    </li>
+                    <?php else:  // Otherwise, this ?>
+                    <li class="register-link">
+                        <a href="register">Join us</a> or <a href="auth">Log in</a>
+                    </li>
+                    <?php endif; ?>
+            
+            </li>
+        </ul>
         <div class="clear"></div>
         
     </div> <!-- branding -->
--- www/sites/default/views/welcome.php
+++ www/sites/default/views/welcome.php
@@ -1,16 +1,12 @@
-<head>
-<?= isset($styles) ? Sourcemap_CSS::link_tags($styles) : Sourcemap_CSS::link_tags(
-        'sites/default/assets/styles/reset.css',
-        'sites/default/assets/styles/general.less'
-    ) ?>
-</head>
-
+<div class="row">
 <?php foreach($supplychains as $id => $details): ?>
+    
 <?php extract((array)$details); ?>
 <?php $title = isset($details->attributes->title) ? $details->attributes->title : 'Untitled'; ?>
-<div class="map-thumb" style="background-image: url('map/static/<?= $id ?>');">
-<a href="map/view/<?= $id ?>"><?= HTML::chars($title) ?></a><br />
-created by <a href="user/<?= $user_id ?>"><?= HTML::chars($owner->username) ?></a> at <?= date('H:ia', $created) ?>
-</div>
+    <div class="map-thumb threecol" style="background-image: url('map/static/<?= $id ?>');">
+    <a href="map/view/<?= $id ?>"><?= HTML::chars($title) ?></a><br />
+    created by <a href="user/<?= $user_id ?>"><?= HTML::chars($owner->username) ?></a> at <?= date('H:ia', $created) ?>
+    </div>
 <?php endforeach; ?>
+</div><!-- .row -->
 <div class="clear">&nbsp;</div>

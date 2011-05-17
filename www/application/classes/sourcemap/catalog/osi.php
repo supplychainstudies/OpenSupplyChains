914a8019aa078b4ca871d4507239586dbcc7671c
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-17 15:14:26 +0000

    osi integration
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@615 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/classes/sourcemap/catalog/osi.php
+++ www/application/classes/sourcemap/catalog/osi.php
@@ -1,8 +1,51 @@
 <?php
 class Sourcemap_Catalog_Osi extends Sourcemap_Catalog {
-    public $long_name = 'Open Sustainability dot Info';
-    public $short_name = 'osi';
-    public $attribution = 'http://opensustainability.info';
 
-    public $url = 'http://db.opensustainability.info/SIAPI/search/';
+    const DEFAULT_ACTION = 'search';
+
+    public $action = 'search';
+
+    public $long_name = 'footprinted dot org';
+    public $short_name = 'osi';
+    public $attribution = 'http://footprinted.org';
+
+    public $url = 'http://footprinted.org/api/';
+
+    public $_cache = true;
+    public $_cache_ttl = 72000;
+
+    public function __construct($params=null) {
+        $action = self::DEFAULT_ACTION;
+        if(is_array($params)) {
+            if(isset($params['action'])) {
+                $action = $params['action'];
+                unset($params['action']);
+            }
+        }
+        $this->action = $action;
+    }
+
+    public function get_url() {
+        return $this->url.$this->action.'/';
+    }
+
+    public function get_cache_key() {
+        $ckey = parent::get_cache_key();
+        $ckey .= '-'.$this->action;
+        return $ckey;
+    }
+
+    public function get_metadata() {
+        $meta = parent::get_metadata();
+        $meta['options'] = array(
+            'action' => array(
+                'required' => 'false',
+                'values' => array(
+                    'search', 'category'
+                ),
+                'default' => 'search'
+            )
+        );
+        return $meta;
+    }
 }

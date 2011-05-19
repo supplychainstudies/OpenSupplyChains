6e9f4bb0512b7a67fd2e7d551015eba0de4737f7
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-19 22:54:13 +0000

    added footer view
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@633 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/sites/default/views/partial/footer.php
@@ -0,0 +1,40 @@
+<div class="container">
+    <div class="row">
+        <div class="threecol">
+            <h3>Test</h3>
+            <ul>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+            </ul>
+        </div>
+        <div class="threecol">
+            <h3>Test</h3>
+            <ul>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+            </ul>
+        </div>
+        <div class="threecol">
+            <h3>Test</h3>
+            <ul>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+            </ul>
+        </div>
+        <div class="threecol last">
+            <h3>Test</h3>
+            <ul>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+                <li>Test</li>
+            </ul>
+        </div>
+    </div><!-- .row -->
+</div><!-- .container -->

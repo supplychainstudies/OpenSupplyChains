a37f7eb0a255d06e4b1ccc56e679a2e7567f5db4
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-19 16:02:56 +0000

    added live search code
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@625 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/sites/default/assets/scripts/search.js
@@ -0,0 +1,30 @@
+// AJAX Search functionality
+// Based on code from http://www.w3schools.com/php/php_ajax_livesearch.asp
+
+function showResult(str)
+{
+if (str.length==0)
+  { 
+  document.getElementById("livesearch").innerHTML="";
+  document.getElementById("livesearch").style.border="0px";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
+    document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+xmlhttp.open("GET","livesearch.php?q="+str,true);
+xmlhttp.send();
+}

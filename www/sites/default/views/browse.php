9baf85c9ee946ab86894ae8595b5a41d73a0ccbb
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-22 20:46:12 +0000

    updates to footer and browse pages
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@649 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/browse.php
+++ www/sites/default/views/browse.php
@@ -1,24 +1,60 @@
-
 <div class="clear"></div>
 <div class="container">
     <div class="row">
-    <?php // Get featured stuff 
-    $data=Sourcemap_Search::Find(array('l'=>3));
-    $results = $data->results;
-    $i = 0;
-    foreach($results as $item){
-    ?>
-        <div class="map-item fourcol<?php if($i == 2): ?> last<? endif; ?>">
-        <img src="/map/static/<?php print $item->id; ?>.s.png" alt="" />
-        <?php
-        print "<br />";
-        print date("Y-m-d",$item->created); 
-        print "<br />";
-        print "<br />";
-        print "</div>";
-        $i++;
-    }
-    ?>
+        <?php // Return three featured items
+        $data=Sourcemap_Search::Find(array('l'=>3));
+        $results = $data->results;
+        $i = 0;
+        foreach($results as $item):
+        ?>
+            <div class="map-item fourcol<?php if($i == 2): ?> last<? endif; ?>">
+            <img src="/map/static/<?php print $item->id; ?>.s.png" alt="" />
+            <br />
+            <?php print date("Y-m-d",$item->created);?>
+            <br />
+            </div>
+            <?php $i++;
+        endforeach;
+        ?>
+    </div><!-- .row -->
+</div><!-- .container -->
 
+<div class="container">
+    <div class="row">
+        <div class="fourcol">
+            <?php // Return columns of category items
+            $data=Sourcemap_Search::Find(array('l'=>3));
+            $results = $data->results;
+            $i = 0;
+            foreach($results as $item):
+            ?>
+                <div class="map-item">
+                <img src="/map/static/<?php print $item->id; ?>.t.png" alt="" />
+                <br />
+                <?php print date("Y-m-d",$item->created);?>
+                <br />
+                </div>
+                <?php $i++;
+            endforeach;
+            ?>
+        </div><!-- .fourcol -->
+        
+        <div class="fourcol">
+            <?php // Return columns of category items
+            $data=Sourcemap_Search::Find(array('l'=>3));
+            $results = $data->results;
+            $i = 0;
+            foreach($results as $item):
+            ?>
+                <div class="map-item">
+                <img src="/map/static/<?php print $item->id; ?>.t.png" alt="" />
+                <br />
+                <?php print date("Y-m-d",$item->created);?>
+                <br />
+                </div>
+                <?php $i++;
+            endforeach;
+            ?>
+        </div><!-- .fourcol -->
     </div><!-- .row -->
 </div><!-- .container -->

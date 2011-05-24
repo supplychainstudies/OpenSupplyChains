f02df7abf3122ec9c881003f869e00491f8881f2
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-24 14:43:47 +0000

    thumb placeholder size fix
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@658 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/classes/controller/map.php
+++ www/sites/default/classes/controller/map.php
@@ -98,7 +98,7 @@ class Controller_Map extends Sourcemap_Controller_Layout {
                     } elseif($pimg = imagecreatefrompng(self::placeholder_image())) {
                         // pass
                         $pimgw = imagesx($pimg); $pimgh = imagesy($pimg);
-                        $rpimgw = $szdim[0]; $rpimgh = $szdim[0]*($pimgh/$pimgw);
+                        $rpimgw = $szdim[2] - $szdim[0]; $rpimgh = $szdim[3] - $szdim[1];
                         $rpimg = imagecreatetruecolor($rpimgw, $rpimgh);
                         imagecopyresampled($rpimg, $pimg, 0, 0, 0, 0, $rpimgw, $rpimgh, $pimgw, $pimgh);
                         imagedestroy($pimg);

622e044994958307863929da697a5bf154d6a9b1
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-24 21:31:17 +0000

    organicvalley demo
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@670 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/assets/scripts/map/embed.js
+++ www/sites/default/assets/scripts/map/embed.js
@@ -1,25 +1,22 @@
 $(document).ready(function() {
 
     Sourcemap.embed_params = Sourcemap.embed_params || {};
-    if(Modernizr && Modernizr.backgroundsize) {
-        Sourcemap.embed_instance = new Sourcemap.Map.Embed(Sourcemap.embed_params);
-        Sourcemap.listen("map:supplychain_mapped", function(evt, map, sc) {
-            var embed = Sourcemap.embed_instance;
-            embed.user_loc = Sourcemap.embed_params.iploc ? Sourcemap.embed_params.iploc[0] : false;
-            if(embed.options.locate_user) {
-                if(embed.tour) embed.tour.stop();
-                embed.showLocationDialog();
-            }
-        });
+    Sourcemap.embed_instance = new Sourcemap.Map.Embed(Sourcemap.embed_params);
 
-        // get scid from inline script
-        var scid = Sourcemap.embed_supplychain_id;
+    Sourcemap.listen("map:supplychain_mapped", function(evt, map, sc) {
+        var embed = Sourcemap.embed_instance;
+        embed.user_loc = Sourcemap.embed_params.iploc ? Sourcemap.embed_params.iploc[0] : false;
+        if(embed.options.locate_user) {
+            if(embed.tour) embed.tour.stop();
+            embed.showLocationDialog();
+        }
+    });
 
-        // fetch supplychain
-        Sourcemap.loadSupplychain(scid, function(sc) {
-            Sourcemap.embed_instance.map.addSupplychain(sc);
-        });
-    } else {
-        $(document.body).html("We're working working on it.");
-    }
+    // get scid from inline script
+    var scid = Sourcemap.embed_supplychain_id;
+
+    // fetch supplychain
+    Sourcemap.loadSupplychain(scid, function(sc) {
+        Sourcemap.embed_instance.map.addSupplychain(sc);
+    });
 });

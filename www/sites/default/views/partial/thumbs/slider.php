5f0d0bae93d9bec02f1f338873c5dfc7fe7acd1f
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-05-31 22:29:15 +0000

    added missing slider files
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@690 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/sites/default/views/partial/thumbs/slider.php
@@ -0,0 +1,29 @@
+<?php // Display the three featured items ?>
+    <ul id="slider">
+<?php 
+$data=Sourcemap_Search::Find(array('l'=>3));
+if ($data):
+    $results = $data->results;
+    $i = 0;
+    foreach($results as $item):
+    ?>
+        <li>
+            <div class="map">
+            <a href="/map/view/<?php print $item->id; ?>">
+            <img class="medium" src="/map/static/<?php print $item->id; ?>.m.png" alt="" />
+            <div class="description">    
+            <h2><?php foreach ($item->attributes as $attribute){ print $attribute; } ?></a></h2>
+            <p>
+            created by <a href="user/<?php print $item->owner->id; ?>"><?php print $item->owner->name; ?></a> 
+            <br />
+            on <?php print date("F j, Y",$item->created);?></h5>
+            <br />
+            </div>
+        </li>
+        <?php $i++;
+    endforeach;
+else:
+    print "<h2>No maps have been created yet.</h2>";
+endif;
+?>
+</ul>

ad7da4ed21190ddef403efb7a2e4f4aeda2ffa2f
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-01 15:34:13 +0000

    improved search results
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@695 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/branding.php
+++ www/sites/default/views/partial/branding.php
@@ -6,20 +6,20 @@
                 </header>
             </a>
         </div>
-        <div id="header-search-div" class="grid_8">
+        <div id="header-search-div" class="grid_7">
             <form method="post" action="/search/">
                 <input id="header-search" type="search" results="0" placeholder="Search" />
                 <div id="livesearch"></div>
             </form>
         </div>
         <nav id="header-nav">
-            <div class="grid-4">
+            <div class="grid-5">
                 <ul>
                     <li>
                         <a href="browse">Browse</a>            
                     </li>
                     <li>
-                        <a href="info">Info</a>
+                        <a href="create">Create</a>
                     </li>
                     <li>
                         <?php if($current_user = Auth::instance()->get_user()): // This happens if the user is logged in ?>

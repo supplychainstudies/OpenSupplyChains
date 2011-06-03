b92d14a432da6092c829ae096e801a4c0fc399a3
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-03 22:11:14 +0000

    added randomized dots to slider
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@707 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/assets/scripts/welcome.js
+++ www/sites/default/assets/scripts/welcome.js
@@ -6,5 +6,30 @@ $(function () {
         hashtags: false,
         width: '960px',
         height: '356px',
+        onInitialized : function(){      // Random styling of buttons 
+            var minSize = 25;
+            var maxSize = 35;
+
+            function randOrd() {
+                return (Math.round(Math.random())-0.5); 
+            }
+
+            var classes = ['orange', 'yellow', 'brown'];
+            classes.sort( randOrd );
+            $(".anythingControls li a").each(function(i){
+                var size = Math.floor(Math.random() * (maxSize - minSize + 1) + minSize);
+                var opacity = Math.random() * .3 + .7;
+                $(this).addClass(classes[i]).css({
+                    'height': size,
+                    'width': size,
+                    'border-radius': size / 2,
+                    '-moz-border-radius': size / 2,
+                    '-webkit-border-radius': size / 2,
+                    'opacity': opacity, 
+                    'padding': 0,
+                    'text-indent': '-999em',
+               });
+            })
+        },
     });
 });

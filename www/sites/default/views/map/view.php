d267b0a86d87faf121e468e1aed90eac81bdbc3f
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-13 13:40:44 +0000

    comments, map layout, etc.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@751 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/map/view.php
+++ www/sites/default/views/map/view.php
@@ -1,1 +1,55 @@
-<div id="sourcemap-map-view" style="width: 100%; height: 100%; background-color: #ddd;"></div>
+<div id="map">
+    <div id="sourcemap-map-view" style="width: 100%; height: 100%; background-color: #ddd;"></div>
+</div>
+    <div class="clear"></div>
+
+    <div class="container_16">
+        <div class="grid_10"> 
+            <div id="map-secondary-content">
+                <div id="discussion-section">
+                    <h3>Comment</h3>
+                    <div id="comment-form">
+                        <textarea id="comment-area"></textarea>
+                    </div>
+                    <input id="comment-submit" type="submit" text="Comment"/>
+                    <div class="clear"></div>
+                    <?php if($comments): ?>
+                    <ul id="comments">
+                        <?php foreach($comments as $i => $comment): ?>
+                            <li class="comment">
+                            <?= View::factory('partial/comment') ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php else: ?>
+                    <h4 class="bad-news">Nobody's commented on this map yet. Be the first!</h4>
+                    <?php endif; ?>
+                </div><!-- #discussion-section -->
+            </div><!-- #map-secondary-content -->
+        </div><!-- .grid_10 -->
+        <div class="grid_6"> 
+            <div id="qrcode-badge">
+                <img class="qrcode" src="<?= $qrcode_url ?>" />
+                <div class="qrcode-about">You can share this physically with a qrcode.</div>
+                <div class="clear"></div>
+            </div>
+            <!--h3>Similar Supplychains</h3>
+            <ul id="similar-supplychains">
+                <li><a href="">Ikea Product Map</a></li>
+                <li><a href="">Ikea Product Map with details.</a></li>
+                <li><a href="">Ikea Product Map</a></li>
+                <li><a href="">Ikea Product Map with details.</a></li>
+                <li><a href="">Ikea Product Map with details.</a></li>
+            </ul>
+            <h3>Related Geographies</h3>
+            <ul id="related-geographies">
+                <li><a href="">Ikea Product Map</a></li>
+                <li><a href="">Ikea Product Map with details.</a></li>
+                <li><a href="">Ikea Product Map</a></li>
+                <li><a href="">Ikea Product Map with details.</a></li>
+                <li><a href="">Ikea Product Map with details.</a></li>
+            </ul-->
+        </div><!-- .grid_6 -->
+    </div><!-- .container -->
+    <div class="clear"></div>
+</div>

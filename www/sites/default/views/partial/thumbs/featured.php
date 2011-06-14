203f6059b506474d94f578ad8f4abaa3e6bf5c70
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-14 18:39:15 +0000

    more styles; mostly home page
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@774 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/thumbs/featured.php
+++ www/sites/default/views/partial/thumbs/featured.php
@@ -1,22 +1,19 @@
-<div class="container_16">
+<div class="container_12">
 <?php $data = Sourcemap_Search::Find(array('l'=>3)); ?>
 <?php if ($data): ?>
     <?php foreach($data->results as $i => $item): ?>
-        <div class="map-item grid_5">
+        <div class="map-item grid_4">
         <a href="/map/view/<?php print $item->id; ?>">
         <img class="small" src="/map/static/<?php print $item->id; ?>.s.png" alt="" />
         </a>
         <br />
-        <h4><?= isset($item->attributes->title) ? $item->attributes->title : "Unknown Name" ?></h4>
-        <h5>By <?= isset($item->owner->name) ? $item->owner->name : "Unknown Owner" ?>
+        <h3><?= isset($item->attributes->title) ? $item->attributes->title : "Unknown Name" ?></h3>
+        <h4 class="author">By <?= isset($item->owner->name) ? $item->owner->name : "Unknown Owner" ?>
         <br/>
-        Published <?php print date("F j, Y",$item->created);?></h5>
+        Published <?php print date("F j, Y",$item->created);?></h4>
         <br />
         </a>
         </div>
-        <?php if($i<2): ?>
-            <div class="grid_1">&nbsp;</div>
-        <?php endif; ?>
     <?php endforeach; ?>
 <?php else: ?>
     <h2>No maps have been created yet.</h2>
--- www/sites/default/views/welcome.php
+++ www/sites/default/views/welcome.php
@@ -7,16 +7,15 @@
 
 <div class="spacer"></div>
 
-<div class="container_16 center">
-    <div class="grid_11">
+<div class="container_12 center">
+    <div class="grid_8">
         <h1>See where things <span class="highlight">come from</span>.</h1>
-        <p>Simply put, we believe that people have the right to know where things come from, what they're made of, and how they affect the environment.  Using freely available data, Sourcemap helps you understand these complex interactions at a glance.</p>  
+        <p>We believe that people have the right to know where things come from, what they're made of, and how they affect the environment.  Using freely available data, Sourcemap helps you understand these complex interactions at a glance.</p>  
     </div>
-    <div class="grid_5">
+    <div class="grid_4">
         <h1>&nbsp;</h1>
         <div class="aside">
-            <p>Contribute to the world's first free and open carbon mapping application.</p>
-            <p class="button"><a href="/register">Create an Account</a></p>
+            <p class="button"><a href="/register">Make an Account</a></p>
             <p class="small">(Already have one? <a href="/login">Sign in.</a>)</p>
         </div>
     </div>
@@ -47,17 +46,17 @@
     <div class="grid_1">&nbsp;</div>
     <div class="grid_6">
         <?php if($news): ?>
-            <h3 class="section">News From Headquarters</h3>
+            <h2>News From Headquarters</h2>
             <div class="news">
                 <ul>
                     <?php foreach($news as $i => $news_item): ?>
                     <li class="news-item">
                         <img src="assets/images/favicon.ico" alt="" />
                         <h5 class="title"><?= HTML::chars($news_item->title) ?></h5>
+                        <div class="clear"></div>
                         <p>
-                            
                             <?= substr(HTML::chars($news_item->body), 0, 140) ?><br />
-                            <a href="http://blog.sourcemap.com">&raquo Read more</a>
+                            <a class="readmore" href="http://blog.sourcemap.com">&raquo Read more</a>
                         </p>
                     </li>
                     <?php endforeach; ?>

3d8ab97a2a984702f8f69b22b3b3f5c512b5d137
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-15 19:41:32 +0000

    search results
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@783 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/search.php
+++ www/sites/default/views/search.php
@@ -1,13 +1,21 @@
-<div class="grid search-results">
+<div class="search-results">
+    <div class="gradient-background">
+        <div class="container_16">
+            <div class="grid_16">
+                <div class="spacer"></div>
+                <h2>Search Results for "<?php if(isset($search_result->parameters['q'])) : HTML::chars($search_result->parameters['q']); endif; ?>"</h2>
+            </div>
+        </div>
+    </div>
+    <div class="clear"></div>
     <div class="container_16">
-    <h1>Search Results for "<?= HTML::chars($search_result->parameters['q']) ?>"</h1>
     </div>
     <?php if(isset($search_result->results) && $search_result && $search_result->results): ?>
         <!--div class="container_16 pager">
             <?= $pager->render() ?>
         </div-->
         <?php foreach($search_result->results as $i => $result): ?>
-            <div class="container_16 search-result">
+            <div class="container_16 map-item">
                 <?= View::factory('partial/search/result', array('result' => $result)) ?>
             </div>
         <?php endforeach; ?>

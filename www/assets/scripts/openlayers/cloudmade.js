b96b00c336f598c6fa6d073d6ee17b7f8d5eeeee
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-16 22:00:57 +0000

    route wrapping
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@795 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/assets/scripts/openlayers/cloudmade.js
+++ www/assets/scripts/openlayers/cloudmade.js
@@ -5,14 +5,22 @@ OpenLayers.Layer.CloudMade = OpenLayers.Class(OpenLayers.Layer.TMS, {
 		}
         options = OpenLayers.Util.extend({
             attribution: "Data &copy; 2009 <a href='http://openstreetmap.org/'>OpenStreetMap</a>. Rendering &copy; 2009 <a href='http://cloudmade.com'>CloudMade</a>.",
-            maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+            maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037300.34,20037508.34),
             maxResolution: 156543.0339,
             units: "m",
             projection: "EPSG:900913",
 			isBaseLayer: true,
-			numZoomLevels: 19,
-			displayOutsideMaxExtent: true,
-			wrapDateLine: true,
+            restrictedMinZoom: 2,
+            tilesize: 256,
+            resolutions: [
+                156543.0339, 78271.51695, 39135.758475, 19567.8792375, 9783.93961875, 
+                4891.969809375, 2445.9849046875, 1222.99245234375, 611.496226171875, 
+                305.7481130859375, 152.87405654296876, 76.43702827148438, 
+                38.21851413574219, 19.109257067871095, 9.554628533935547, 
+                4.777314266967774, 2.388657133483887, 1.1943285667419434, 0.5971642833709717
+            ],
+			displayOutsideMaxExtent: false,
+			wrapDateLine: false,
 			styleId: 1
         }, options);
 		var prefix = [options.key, options.styleId, 256].join('/') + '/';

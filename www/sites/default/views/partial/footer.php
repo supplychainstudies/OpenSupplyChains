1e3fe4a6ee6ef39407e5ceb38ee88bad2d176b03
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-16 17:41:21 +0000

    Styling.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@791 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/footer.php
+++ www/sites/default/views/partial/footer.php
@@ -1,30 +1,19 @@
-<div class="footer gradient-background">
+<div class="footer">
     <div class="container_16">
-        <div class="grid_5">
-            <h3>Information</h3>
+        <div class="grid_16">
             <ul>
                 <li><a href="info">About Sourcemap</a></li>                
                 <li><a href="info/help">Help and FAQ</a></li> 
                 <li><a href="info/api">API and Code</a></li> 
 
-            </ul>
-        </div>
-        <div class="grid_6">
-            <h3>News & Contact</h3>
-            <ul>
                 <li><a href="/info/contact">Get In Touch</a></li>                   
                 <li><a href="http://blog.sourcemap.com">Sourcemap Blog</a></li> 
                 <li><a href="http://blog.sourcemap.com/category/press-releases">Press Releases</a></li> 
-            </ul>
-        </div>
-        <div class="grid_5">
-            <h3>Privacy & Use</h3>
-            <ul>
+          
                 <li><a href="info/privacy">Privacy Policy</a></li>
                 <li><a href="info/terms">Terms of Service</a></li>
-                <li><a href="info/dmca">DMCA</a></li>                
-            </ul>
-        </div>
+                <li><a href="info/dmca">DMCA</a></li>    
+            </ul>            
     </div>
     <div class="clear"></div>
 </div>

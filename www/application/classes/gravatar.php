a89faaafbcb4d5d08a891e75cad12ebe1a05cb9f
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-18 21:58:36 +0000

    gravatar default
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@815 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/classes/gravatar.php
+++ www/application/classes/gravatar.php
@@ -3,7 +3,7 @@ class Gravatar {
 
     public static $_avatar_base = 'http://www.gravatar.com/avatar/%s?d=%s&s=%d'; // identicon | retro
 
-    public static function avatar($email, $sz=64, $d='identicon') {
+    public static function avatar($email, $sz=64, $d='mm') {
         $sz = min(512, max(1, $sz), $sz);
         return sprintf(self::$_avatar_base, self::hash($email), urlencode($d), $sz);
     }

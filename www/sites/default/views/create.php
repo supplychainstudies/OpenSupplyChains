b5bcf294ab499798c222e4e1ec116347da857bc1
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-21 20:37:09 +0000

    form styles and misc. fixes
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@840 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/create.php
+++ www/sites/default/views/create.php
@@ -1,10 +1,6 @@
-<div class="grids">
-    <div class="container_16">
-        <div class="form">
-            <fieldset>
-            <legend>Create a New Sourcemap</legend>
-                <?= $create_form ?>
-            </fieldset>
-        </div>
-    </div>
+<div class="form">
+    <fieldset>
+    <legend>Create a New Sourcemap</legend>
+        <?= $create_form ?>
+    </fieldset>
 </div>
--- www/sites/default/views/map/view.php
+++ www/sites/default/views/map/view.php
@@ -1,64 +1,62 @@
 <div id="map">
     <div id="sourcemap-map-view" style="width: 100%; height: 100%; background-color: #ddd;"></div>
 </div>
-    <div class="spacer"></div>
-        <div class="container_16">
-            <div class="grid_8">
-            <h2>Discussion</h2>
-            </div>
-        </div>
+<div class="spacer"></div>
+<div class="container_16">
+    <div class="grid_8">
+        <h2>Discussion</h2>
     </div>
-    <div class="container_16">
-        <div class="grid_10"> 
-            <div id="map-secondary-content">
-                <div id="discussion-section">
-                    <?php if($can_comment): ?>
-                    <div id="comment-form" class="form">
-                        <form method="post" action="map/comment/<?= $supplychain_id ?>">
-                            <textarea placeholder="Submit your comment." name="body" id="comment-area"></textarea>
-                            <input id="comment-submit" type="submit" text="Comment"/>
-                        </form>
-                        </fieldset>
-                    </div>
-                    <div class="clear"></div>
-                    <?php endif; ?>
-                    <?php if($comments): ?>
-                    <ul id="comments">
-                        <?php foreach($comments as $i => $comment): ?>
-                            <li class="comment">
-                            <?= View::factory('partial/comment', array('comment' => $comment)) ?>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <?php else: ?>
-                    <h4 class="bad-news">Nobody's commented on this map yet.</h4>
-                    <?php endif; ?>
-                </div><!-- #discussion-section -->
-            </div><!-- #map-secondary-content -->
-        </div><!-- .grid_10 -->
-        <div class="grid_6">
-            <div id="qrcode-badge">
-                <img class="qrcode" src="<?= $qrcode_url ?>" />
-                <div class="qrcode-about">You can share this physically with a qrcode.</div>
-                <div class="clear"></div>
-            </div>
-            <!--h3>Similar Supplychains</h3>
-            <ul id="similar-supplychains">
-                <li><a href="">Ikea Product Map</a></li>
-                <li><a href="">Ikea Product Map with details.</a></li>
-                <li><a href="">Ikea Product Map</a></li>
-                <li><a href="">Ikea Product Map with details.</a></li>
-                <li><a href="">Ikea Product Map with details.</a></li>
-            </ul>
-            <h3>Related Geographies</h3>
-            <ul id="related-geographies">
-                <li><a href="">Ikea Product Map</a></li>
-                <li><a href="">Ikea Product Map with details.</a></li>
-                <li><a href="">Ikea Product Map</a></li>
-                <li><a href="">Ikea Product Map with details.</a></li>
-                <li><a href="">Ikea Product Map with details.</a></li>
-            </ul-->
-        </div><!-- .grid_6 -->
-    </div><!-- .container -->
-    <div class="clear"></div>
 </div>
+<div class="container_16">
+    <div class="grid_10"> 
+        <div id="map-secondary-content">
+            <div id="discussion-section">
+                <?php if($can_comment): ?>
+                <div id="comment-form" class="form">
+                    <form method="post" action="map/comment/<?= $supplychain_id ?>">
+                        <textarea placeholder="Submit your comment." name="body" id="comment-area"></textarea>
+                        <input id="comment-submit" type="submit" text="Comment"/>
+                    </form>
+                    </fieldset>
+                </div>
+                <div class="clear"></div>
+                <?php endif; ?>
+                <?php if($comments): ?>
+                <ul id="comments">
+                    <?php foreach($comments as $i => $comment): ?>
+                        <li class="comment">
+                        <?= View::factory('partial/comment', array('comment' => $comment)) ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php else: ?>
+                <h4 class="bad-news">Nobody's commented on this map yet.</h4>
+                <?php endif; ?>
+            </div><!-- #discussion-section -->
+        </div><!-- #map-secondary-content -->
+    </div><!-- .grid_10 -->
+    <div class="grid_6">
+        <div id="qrcode-badge">
+            <img class="qrcode" src="<?= $qrcode_url ?>" />
+            <div class="qrcode-about">You can share this physically with a qrcode.</div>
+            <div class="clear"></div>
+        </div>
+        <!--h3>Similar Supplychains</h3>
+        <ul id="similar-supplychains">
+            <li><a href="">Ikea Product Map</a></li>
+            <li><a href="">Ikea Product Map with details.</a></li>
+            <li><a href="">Ikea Product Map</a></li>
+            <li><a href="">Ikea Product Map with details.</a></li>
+            <li><a href="">Ikea Product Map with details.</a></li>
+        </ul>
+        <h3>Related Geographies</h3>
+        <ul id="related-geographies">
+            <li><a href="">Ikea Product Map</a></li>
+            <li><a href="">Ikea Product Map with details.</a></li>
+            <li><a href="">Ikea Product Map</a></li>
+            <li><a href="">Ikea Product Map with details.</a></li>
+            <li><a href="">Ikea Product Map with details.</a></li>
+        </ul-->
+    </div><!-- .grid_6 -->
+</div><!-- .container -->
+<div class="clear"></div>

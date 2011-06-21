c34f9ec8136216f5d23a60603e1289c6a3c33039
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-21 21:42:03 +0000

    okay the form styles are better now
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@843 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/register.php
+++ www/sites/default/views/register.php
@@ -1,3 +1,34 @@
+<?php if(!isset($current_user) || !$current_user): ?>
+<div class="container_16">
+<div class="grid_9">
+<div class="sourcemap-form">
+  <fieldset> 
+    <legend>Log in</legend> 
+    <form name="register" class="form" method="post" action="auth/login">
+        <label for="textbox">Username</label>
+        <div class="sourcemap-form-textbox"> 
+            <input name="username" type="text" class="username" id="username" value="username" /> 
+        </div> 
+        <label for="password">Password</label> 
+        <div class="sourcemap-form-textbox"> 
+            <input name="password" type="password" class="password" id="password" value="password" /> 
+        </div> 
+        <?php if (isset($_GET['next'])): ?>
+            <input type="hidden" name="next" value="<?= $_GET['next']; ?>" />
+        <?php endif; ?>
+        <div class="sourcemap-form-button-div"> 
+            <input name="Submit" type="submit" value="Submit" class="buttons" /> 
+        </div> 
+    </form>
+    </fieldset>
+</div>
+</div>
+</div>
+
+<?php else: ?>
+<h2>You&apos;re logged in as <?= $current_user->username ?>.</h2>
+<?php endif; ?>
+
 <div class="container_16">
     <div class="grid_6">
     <h2>Register</h2>

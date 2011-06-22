02a6dd850d8c5bd6ed46c6647704e15c4839a895
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-22 21:17:56 +0000

    browse facets
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@852 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/thumbs/featured.php
+++ www/sites/default/views/partial/thumbs/featured.php
@@ -1,24 +1,35 @@
 <div id="popular-maps" class="container_12">
-
-<?php $data=Sourcemap_Search::Find(array('l'=>3));
-if ($data):
-    $results = $data->results;
-    $i = 0;
-    foreach($results as $item):?>
+<?php if($supplychains): ?>
+    <?php foreach($supplychains as $i => $item):?>
         <div class="preview-map-item medium grid_4">
             <div class="preview-badge">
-            <a href="/map/view/<?php print $item->id; ?>"><img class="preview-map small" src="/map/static/<?php print $item->id; ?>.s.png" alt="" /></a>
+            <a href="view/<?php print $item->id; ?>"><img class="preview-map small" src="static/<?= $item->id ?>.s.png" alt="" /></a>
             </div>
-            <h3 class="preview-title"><a href="/map/view/<?php print $item->id; ?>"><?= isset($item->attributes->title) ? $item->attributes->title : "Unknown Name" ?></a></h3>
-            <h4 class="preview-author">By <a href="user/<?php $item->owner->id; ?>"><?= isset($item->owner->name) ? $item->owner->name : "Unknown Owner" ?></a>,
-            <?php print date("F j, Y",$item->created);?></h4>
-            <? if(isset($item->teaser)) { ?><p class="preview-teaser"><?= $item->teaser; ?></p><? } ?>
+            <h3 class="preview-title">
+                <a href="view/<?= $item->id; ?>">
+                    <?php if(isset($item->attributes->title)): ?>
+                        <?= HTML::chars($item->attributes->title) ?>
+                    <?php elseif(isset($item->attributes->name)): ?>
+                        <?= HTML::chars($item->attributes->name) ?>
+                    <?php else: ?>
+                        An Unnamed Sourcemap
+                    <?php endif; ?>
+                </a></h3>
+            <h4 class="preview-author">By <a href="user/<?php $item->owner->id; ?>">
+                <?php if(isset($item->owner->name)): ?>
+                    <?= HTML::chars($item->owner->name) ?>
+                <?php else: ?>
+                    Unknown Owner
+                <?php endif; ?>
+                </a>,
+            <?= print date("F j, Y", $item->created) ?></h4>
+            <?php if(isset($item->teaser)): ?><p class="preview-teaser"><?= HTML::chars($item->teaser) ?></p><?php endif; ?>
             
         </div>
     <?php endforeach; ?>
 <?php else: ?>
-    <div class="preview-map-item medium grid_4">    
-        <h3>No maps have been created yet.</h3>
+    <div class="preview-map-item medium grid_4">
+        --
     </div>
 <?php endif; ?>
 </div>

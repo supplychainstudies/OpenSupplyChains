dbeeca7816920d4b4d6ef706cb11676a88e2df10
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-24 16:15:45 +0000

    style changes, form improvements
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@865 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/register.php
+++ www/sites/default/views/register.php
@@ -1,8 +1,18 @@
 <?php if(!isset($current_user) || !$current_user): ?>
 
 <div class="container_16">
+    <div class="grid_10">
+        <h1>Register with Sourcemap</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet ultrices neque.</p>
+        <p>In pulvinar ipsum quis orci ornare non lobortis turpis tincidunt. Integer at ligula erat, in hendrerit risus. Vivamus mattis</p>
+        <p>Fusce ultricies pharetra eros, nec ornare magna auctor blandit. Fusce non risus non odio elementum mollis.</p>
+        <div class="spacer">&nbsp;</div>
+        <ul>
+            <li>Already have an account? <a href="/auth">Sign in here</a>.</li>
+        </ul>
+    </div>
     <div class="grid_6">
-    <h2>Register</h2>
+        <h1>&nbsp;</h1>
         <?php // Create form using the Sourcemap_Form class
         $f = Sourcemap_Form::factory('register')
             ->method('post')

4bcfee302ffe588b42f8ab39fb6453a4e68c43dc
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-27 20:59:20 +0000

    default feature color
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@888 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/assets/scripts/sourcemap/map.js
+++ www/assets/scripts/sourcemap/map.js
@@ -29,7 +29,7 @@ Sourcemap.Map.prototype.defaults = {
     "popup_height": 100, "animation_enabled":false,
     "draw_hops": true, "hops_as_arcs": true,
     "hops_as_bezier": false, "arrows_on_hops": true,
-    "default_feature_color": "#898989",
+    "default_feature_color": "#595959",
     "stop_style": {
         "default": {
             "pointRadius": "${size}",

4ec1c6c04f68fb9a1ac0c5fdc6e93eb07426d5aa
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-29 17:10:16 +0000

    welcome page recent map fixes
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@906 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/classes/controller/welcome.php
+++ www/sites/default/classes/controller/welcome.php
@@ -22,8 +22,13 @@ class Controller_Welcome extends Sourcemap_Controller_Layout {
         $this->layout->styles[] = 'sites/default/assets/styles/slider.css';
         
         $this->layout->page_title = 'Welcome to Sourcemap.';
-        $recent = Sourcemap_Search::find(array('recent' => 'yes', 'l' => 12));
-        $this->template->recent = $recent->results;
+        $recent1 = Sourcemap_Search::find(array('recent' => 'yes', 'l' => 3));
+        $recent2 = Sourcemap_Search::find(array('recent' => 'yes', 'o' => 3, 'l' => 3));
+        $recent3 = Sourcemap_Search::find(array('recent' => 'yes', 'o' => 6, 'l' => 3));
+
+        $this->template->recent1 = $recent1->results;
+        $this->template->recent2 = $recent2->results;
+        $this->template->recent3 = $recent3->results;
 
         $featured = Sourcemap_Search::find(array('featured' => 'yes', 'l' => 12));
         $this->template->featured = $featured;
@@ -47,6 +52,6 @@ class Controller_Welcome extends Sourcemap_Controller_Layout {
         $this->template->popular = Sourcemap_Search_Simple::find($iparams);
 
         $this->template->news = Blognews::fetch(4);
-        $this->template->supplychains = $recent;
+        $this->template->supplychains = $recent1;
     }
 }

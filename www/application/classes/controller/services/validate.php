5d4396573aba52767833fc81ecac172ada09b5b5
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-06-30 19:35:28 +0000

    form validation svc
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@913 ab0f7204-c6de-4229-9497-6c0baa3b4d53

---
+++ www/application/classes/controller/services/validate.php
@@ -0,0 +1,20 @@
+<?php
+class Controller_Services_Validate extends Sourcemap_Controller_Service {
+    public function action_post($p=false) {
+        if($p === false) {
+            return $this->_bad_request();
+        }
+
+        $f = Sourcemap_Form::load($p);
+
+        if($f) {
+            if($f->validate((array)$this->request->posted_data)) {
+                $this->response = true;
+            } else {
+                $this->response = (object)$f->errors();
+            }
+        } else {
+            return $this->_not_found('Form no exist.');
+        }
+    }
+}
---
+++ www/application/classes/sourcemap/form/field/hidden.php
@@ -0,0 +1,4 @@
+<?php
+class Sourcemap_Form_Field_Hidden extends Sourcemap_Form_Field_Text {
+    protected $_type = 'hidden';
+}

e4198f8563624f0eda45f0afc8a75104a307a777
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-07-22 16:17:31 +0000

    missing images
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@964 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/footer.php
+++ www/sites/default/views/partial/footer.php
@@ -1,20 +1,19 @@
 <div class="footer">
-    <div class="container_16">
-        <div class="grid_16">
-            <ul>
-                <li><a href="info">About</a></li>                
-                <li><a href="info/help">Help and FAQ</a></li> 
-                <li><a href="info/api">API and Code</a></li> 
+    <div class="container">
+        <ul>
+            <li><a href="info">About</a></li>                
+            <li><a href="info/help">Help and FAQ</a></li> 
+            <li><a href="info/api">API and Code</a></li> 
 
-                <li><a href="/info/contact">Get In Touch</a></li>                   
-                <li><a href="http://blog.sourcemap.com">Blog</a></li> 
-                <li><a href="http://blog.sourcemap.com/category/press-releases">Press Releases</a></li> 
-          
-                <li><a href="info/privacy">Privacy Policy</a></li>
-                <li><a href="info/terms">Terms of Service</a></li>
-                <li><a href="info/dmca">DMCA</a></li>    
-            </ul>   
-            <p>&copy; 2011 Sourcemap Inc.<p>         
+            <li><a href="/info/contact">Get In Touch</a></li>                   
+            <li><a href="http://blog.sourcemap.com">Blog</a></li> 
+            <li><a href="http://blog.sourcemap.com/category/press-releases">Press Releases</a></li> 
+      
+            <li><a href="info/privacy">Privacy Policy</a></li>
+            <li><a href="info/terms">Terms of Service</a></li>
+            <!-- <li><a href="info/dmca">DMCA</a></li> -->    
+        </ul>   
+        <p>&copy; 2011 Sourcemap Inc.<p>         
     </div>
     <div class="clear"></div>
 </div>

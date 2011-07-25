01d60f683d14d585a43fd956db38133a19ede79d
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-07-25 21:19:42 +0000

    reskinning: misc changes
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@971 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/home.php
+++ www/sites/default/views/home.php
@@ -1,15 +1,13 @@
-<div class="container_16">
-    <div class="grid_8">
-        <div id="user-profile">
-            <h2 class="user-name"><?= HTML::chars($user->username) ?></h2>       
-            <img class="user-gravatar" src="<?= Gravatar::avatar($user->email, 128) ?>" />            
-            <ul class="user-details">
-                <li>Last Login: <?= date('F j, Y', $user->last_login) ?><li>
-            </ul>
-            <div class="clear"></div>
-        </div>
+<div class="container">
+    <div id="user-profile">
+        <h2 class="user-name"><?= HTML::chars($user->username) ?></h2>       
+        <img class="user-gravatar" src="<?= Gravatar::avatar($user->email, 128) ?>" />            
+        <ul class="user-details">
+            <li>Last Login: <?= date('F j, Y', $user->last_login) ?><li>
+        </ul>
+        <div class="clear"></div>
     </div>
-    <div class="grid_8 events">
+    <div class="events">
         <div id="user-stream">
             <h2>Recent Activity</h2>
             <?php if(isset($user_event_stream)): ?>
@@ -20,15 +18,15 @@
 </div>
 <div class="clear"></div>
 
-<div class="container_16 search-results">
-        <?php if(isset($supplychains) && $supplychains): ?>
-            <h2>Your maps</h2>
-            <?php foreach($supplychains as $i => $sc): ?>
-                <div id="user-map-list" class="<?= $i % 2 ? " alt" : ''; ?>">
-                    <?= View::factory('partial/home/map', array('supplychain' => $sc)) ?>
-                </div>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <h2 class="bad-news">You haven't made any maps yet. <a href="create">Get started</a></h2>
-        <?php endif; ?>
+<div class="container search-results">
+    <?php if(isset($supplychains) && $supplychains): ?>
+        <h2>Your maps</h2>
+        <?php foreach($supplychains as $i => $sc): ?>
+            <div id="user-map-list" class="<?= $i % 2 ? " alt" : ''; ?>">
+                <?= View::factory('partial/home/map', array('supplychain' => $sc)) ?>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <h2 class="bad-news">You haven't made any maps yet. <a href="create">Get started</a></h2>
+    <?php endif; ?>
 </div>
--- www/sites/default/views/map/view.php
+++ www/sites/default/views/map/view.php
@@ -1,48 +1,40 @@
-<div class="container_16">
+<div id="map-container">
     <div id="map">
         <div id="sourcemap-map-view"></div>
     </div>
 </div>
 <div class="spacer"></div>
-<div class="container_16">
-    <div class="grid_8">
-        <h2>Discussion</h2>
-    </div>
-</div>
-<div class="container_16">
-    <div class="grid_10"> 
-        <div id="map-secondary-content">
-            <div id="discussion-section">
-                <?php if($can_comment): ?>
-                <div id="comment-form" class="form">
-                    <form method="post" action="map/comment/<?= $supplychain_id ?>">
-                        <textarea placeholder="Submit your comment." name="body" id="comment-area"></textarea>
-                        <input id="comment-submit" type="submit" text="Comment"/>
-                    </form>
-                    </fieldset>
-                </div>
-                <div class="clear"></div>
-                <?php endif; ?>
-                <?php if($comments): ?>
-                <ul id="comments">
-                    <?php foreach($comments as $i => $comment): ?>
-                        <li class="comment">
-                        <?= View::factory('partial/comment', array('comment' => $comment)) ?>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-                <?php else: ?>
-                <h4 class="bad-news">Nobody's commented on this map yet.</h4>
-                <?php endif; ?>
-            </div><!-- #discussion-section -->
-        </div><!-- #map-secondary-content -->
-    </div><!-- .grid_10 -->
-    <div class="grid_6">
-        <div id="qrcode-badge">
-            <img class="qrcode" src="<?= $qrcode_url ?>" />
-            <div class="qrcode-about">You can share this physically with a qrcode.</div>
+<div class="container">
+    <h2>Discussion</h2>
+    <div id="map-secondary-content">
+        <div id="discussion-section">
+            <?php if($can_comment): ?>
+            <div id="comment-form" class="form">
+                <form method="post" action="map/comment/<?= $supplychain_id ?>">
+                    <textarea placeholder="Submit your comment." name="body" id="comment-area"></textarea>
+                    <input id="comment-submit" type="submit" text="Comment"/>
+                </form>
+                </fieldset>
+            </div>
             <div class="clear"></div>
-        </div>
-    </div><!-- .grid_6 -->
+            <?php endif; ?>
+            <?php if($comments): ?>
+            <ul id="comments">
+                <?php foreach($comments as $i => $comment): ?>
+                    <li class="comment">
+                    <?= View::factory('partial/comment', array('comment' => $comment)) ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+            <?php else: ?>
+            <h4 class="bad-news">Nobody's commented on this map yet.</h4>
+            <?php endif; ?>
+        </div><!-- #discussion-section -->
+    </div><!-- #map-secondary-content -->
+    <div id="qrcode-badge">
+        <img class="qrcode" src="<?= $qrcode_url ?>" />
+        <div class="qrcode-about">You can share this physically with a qrcode.</div>
+        <div class="clear"></div>
+    </div>
 </div><!-- .container -->
 <div class="clear"></div>

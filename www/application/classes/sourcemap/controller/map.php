1aaad21c7dff7f06f2b1f0719602662e1cc6b260
alex <alex@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-07-27 19:51:22 +0000

    reskinning: map view
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@981 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/application/classes/sourcemap/controller/map.php
+++ www/application/classes/sourcemap/controller/map.php
@@ -22,12 +22,22 @@ class Sourcemap_Controller_Map extends Sourcemap_Controller_Layout {
             $supplychain_id = $this->_match_alias($supplychain_id);
         }
         $supplychain = ORM::factory('supplychain', $supplychain_id);
+        $sc = $supplychain->kitchen_sink($supplychain_id);
         if($supplychain->loaded()) {
             $current_user_id = Auth::instance()->logged_in() ? (int)Auth::instance()->get_user()->id : 0;
             $owner_id = (int)$supplychain->user_id;
             if($supplychain->user_can($current_user_id, Sourcemap::READ)) {
                 $this->layout->supplychain_id = $supplychain_id;
+               
+                // pass supplychain metadeta to template 
                 $this->template->supplychain_id = $supplychain_id;
+                $this->template->supplychain_date = date('F j, Y', $sc->created );
+                $this->template->supplychain_name = isset($sc->attributes->name) ? $sc->attributes->name : "";
+                $this->template->supplychain_owner = isset($sc->owner->name) ? $sc->owner->name : "";
+                $this->template->supplychain_ownerid = isset($sc->owner->id) ? $sc->owner->id : "";
+                $this->template->supplychain_avatar = isset($sc->owner->avatar) ? $sc->owner->avatar : "";
+                $this->template->supplychain_desc = isset($sc->attributes->description) ? $sc->attributes->description : "" ;
+
                 $this->layout->scripts = array('map-view');
                 $this->layout->styles = array(
                     'sites/default/assets/styles/reset.css', 
--- www/sites/default/views/map/view.php
+++ www/sites/default/views/map/view.php
@@ -6,36 +6,68 @@
 </div>
 <div class="spacer"></div>
 <div class="container">
-    <h2>Discussion</h2>
-    <div id="map-secondary-content">
-        <div id="discussion-section">
-            <?php if($can_comment): ?>
-            <div id="comment-form" class="form">
-                <form method="post" action="map/comment/<?= $supplychain_id ?>">
-                    <textarea placeholder="Submit your comment." name="body" id="comment-area"></textarea>
-                    <input id="comment-submit" type="submit" text="Comment"/>
-                </form>
-                </fieldset>
+    <div id="sidebar" class="map-view">
+        <div class="container">
+            <h2>Share this Sourcemap</h2>
+        </div>
+        <hr />
+        <div id="qrcode-badge" class="container">
+            <img class="qrcode" src="<?= $qrcode_url ?>" />
+            <div class="qrcode-about">
+                <p>You can share this physically with a qrcode.</p>
+                <p><em>Print the QR code, or use a QR enabled smart phone to return to this map.</em></p>
             </div>
             <div class="clear"></div>
-            <?php endif; ?>
-            <?php if($comments): ?>
-            <ul id="comments">
-                <?php foreach($comments as $i => $comment): ?>
-                    <li class="comment">
-                    <?= View::factory('partial/comment', array('comment' => $comment)) ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-            <?php else: ?>
-            <h4 class="bad-news">Nobody's commented on this map yet.</h4>
-            <?php endif; ?>
-        </div><!-- #discussion-section -->
-    </div><!-- #map-secondary-content -->
-    <div id="qrcode-badge">
-        <img class="qrcode" src="<?= $qrcode_url ?>" />
-        <div class="qrcode-about">You can share this physically with a qrcode.</div>
-        <div class="clear"></div>
+        </div>
+        <hr />
+        <div class="container links">
+            <p>Link to this Sourcemap</p>
+            <div>
+                <input value="<?= URL::site(NULL, TRUE) ?>view/<?= $supplychain_id ?>" readonly="readonly"></input>
+            </div>
+            <p>Embed this Sourcemap</p>
+            <div>
+                <input value="<?= URL::site(NULL, TRUE) ?>embed/<?= $supplychain_id ?>" readonly="readonly"></input>
+            </div>
+        </div>
+        <hr />
+                
+        <?= View::factory('partial/social', array('supplychain_id' => $supplychain_id)); ?>
+        
     </div>
+    
+    <h1><?= $supplychain_name ?></h1>
+    <p class="description"><?= $supplychain_desc ?></p>
+    
+    <p class="author">
+        <img src="<?= $supplychain_avatar ?>" alt="Avatar"></img>
+        Created on <?= $supplychain_date ?> by <a href="user/<?= $supplychain_ownerid ?>"><?= $supplychain_owner ?></a>
+    </p>
+
+    <div id="discussion-section">
+        <h2>Discussion</h2>
+        <?php if($can_comment): ?>
+        <div id="comment-form" class="form">
+            <form method="post" action="map/comment/<?= $supplychain_id ?>">
+                <textarea placeholder="Submit your comment." name="body" id="comment-area"></textarea>
+                <input id="comment-submit" type="submit" text="Comment"/>
+            </form>
+            </fieldset>
+        </div>
+        
+        <?php endif; ?>
+        <?php if($comments): ?>
+        <ul id="comments">
+            <?php foreach($comments as $i => $comment): ?>
+                <li class="comment">
+                <?= View::factory('partial/comment', array('comment' => $comment)) ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php else: ?>
+        <h4 class="bad-news">Nobody's commented on this map yet.</h4>
+        <?php endif; ?>
+    </div><!-- #discussion-section -->
+    
 </div><!-- .container -->
 <div class="clear"></div>

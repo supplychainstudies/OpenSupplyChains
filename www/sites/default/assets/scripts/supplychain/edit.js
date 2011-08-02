ee099092780ae924ac6cd96724095b6ab2e04333
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-02 15:52:57 +0000

    Cleanup
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1009 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/assets/scripts/supplychain/edit.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    var scid = window.location.href.split('/').pop();
-    Sourcemap.loadSupplychain(scid, function(sc) {
-        var ed = new Sourcemap.TabbedEditor('#supplychain-form-edit', sc);
-    });
-});

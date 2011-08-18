e0e3444f66dcedddeed7b3537e83073f908e1624
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-18 17:49:09 +0000

    tweaks
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1183 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/assets/scripts/sourcemap/map/base.js
+++ www/assets/scripts/sourcemap/map/base.js
@@ -234,8 +234,8 @@ Sourcemap.Map.Base.prototype.showDialog = function(mkup) {
 Sourcemap.Map.Base.prototype.hideDialog = function(notrigger) {
     if(this.dialog) {
         $(this.dialog).hide();
-        this.map.controls["select"].unselectAll();
         if(!notrigger) {
+            this.map.controls["select"].unselectAll();
             Sourcemap.broadcast('sourcemap-base-dialog-close', 
                 this, this.map.editor ? $(this.dialog).find("form").serializeArray() : false
             );

ec377fb20563593b0be1927fb4d5a019f0038120
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-18 20:46:03 +0000

    Some Stuff.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1190 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/auth/forgot_password.php
+++ www/sites/default/views/auth/forgot_password.php
@@ -1,14 +1,24 @@
-<div class="container_16">
-    <div class="sourcemap-form">
-        <fieldset>
-        <legend>Forgot your password?</legend>
-        <form name="create" method="post" action="auth/forgot">
-            <label for="email">Email:</label>
-            <input type="text" id="email" name="email" class="required" value="" />
-            <div class="clear"></div>
-            <input type="submit" class="submit" value="Go!"/>
-        </form>
-        </fieldset>
-        <div><span class="highlighted">*</span> denotes required field</div>
+<div class="container">
+	<div class="login-copy">
+        <h1>Password Request</h1>
+        <p>Forgot your password? Enter the email address you used to register for Sourcemap and we'll send you a link to reset your password.</p><br/>
+        <ul>
+            <li><a href="/auth">Sign in to Sourcemap</a></li>
+            <li><a href="/register">Register a new account</a></li>
+        </ul>
     </div>
+    <div class="login-box">
+	    <h1>&nbsp;</h1>
+	    <div class="sourcemap-form">
+	        <fieldset>
+	        <form name="create" method="post" action="auth/forgot">
+		    	<label for="email">Email:</label>    
+				<div class="sourcemap-form-textbox">
+		            <input type="text" id="email" name="email" class="required" value="" />
+				</div><br/>
+	            <input class="button" type="submit" class="submit" value="Request"/>
+	        </form>
+	        </fieldset>
+	    </div>
+	</div>
 </div>

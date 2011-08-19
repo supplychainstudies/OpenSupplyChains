28947ff6ce27f4427b7deed0181408770b4816f3
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-19 23:15:52 +0000

    ie array fix
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1227 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/assets/scripts/sourcemap.js
+++ www/assets/scripts/sourcemap.js
@@ -1,3 +1,18 @@
+// for starters, let's fix old javascripts.
+// Add ECMA262-5 Array methods if not supported natively
+//
+if (!('indexOf' in Array.prototype)) {
+    Array.prototype.indexOf= function(find, i /*opt*/) {
+        if (i===undefined) i= 0;
+        if (i<0) i+= this.length;
+        if (i<0) i= 0;
+        for (var n= this.length; i<n; i++)
+            if (i in this && this[i]===find)
+                return i;
+        return -1;
+    };
+}
+
 Sourcemap = {};
 _S = Sourcemap;
 

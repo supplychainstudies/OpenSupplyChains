6bfec0295805636120a214cb87748db27577d511
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-19 17:11:48 +0000

    Search page styles.
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1202 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/search/result.php
+++ www/sites/default/views/partial/search/result.php
@@ -1,37 +1,22 @@
-<?php if(isset($result) && $result): ?>
-        <div class="grid_3">
-            <a href="map/view/<?= $result->id ?>">
-                <img class="user-map-preview" src="map/static/<?= $result->id ?>.t.png" />
+<?php if(isset($supplychain) && $supplychain): ?>
+        <div class="map-item small">
+            <a href="map/view/<?= $supplychain->id ?>">
+                <img class="user-map-preview" src="map/static/<?= $supplychain->id ?>.s.png" />
             </a>
         </div>
-        <div class="grid_9 map-item">
-            <a href="map/view/<?= $result->id ?>">
-                <h2 class="user-map-title"><?= isset($result->attributes->title) ? HTML::chars($result->attributes->title) : "An Unnamed Sourcemap" ?></h2>
+        <div class="map-description">
+            <a href="map/view/<?= $supplychain->id ?>">
+                <h2 class="map-title"><?= isset($supplychain->attributes->title) ? HTML::chars($supplychain->attributes->title) : "An Unnamed Sourcemap" ?></h2>
             </a>
+            <h4 class="map-details">
+            	<?= HTML::chars($supplychain->owner->name) ?>, <?= date('F j, Y', $supplychain->created) ?>
+            </h4>
+            <div class="clear"></div>
             
-            <?php if(isset($result->attributes->teaser) && $result->attributes->teaser): ?>
-                <h3 class="user-map-teaser"><?= HTML::chars($result->attributes->teaser) ?></h3>
-            <?php elseif(isset($result->attributes->description) && $result->attributes->description): ?>
-                <h3 class="user-map-teaser"><?= HTML::chars($result->attributes->description) ?></h3>
-            <?php else: ?>
-                <h3 class="user-map-teaser">(No description yet)</h3>
+            <?php if(isset($supplychain->attributes->description) && $supplychain->attributes->description): ?>
+                <span class="map-teaser"><?= HTML::chars($supplychain->attributes->description) ?></span>
             <?php endif; ?>
-            
-            <?php if(isset($result->owner->name) && $result->owner->name): ?>
-                <h4 class="user-map-details">
-                     <?= HTML::chars($result->owner->name) ?>
-                    
-                    <?php if($result->modified > $result->created): ?>
-                        : <?= date('F j, Y', $result->modified) ?> 
-                    <?php endif; ?>
-                    : <?= date('F j, Y', $result->created) ?>
-                </h4>
-            <?php endif; ?>
+        </div>
 
-            <?php if($result->other_perms & Sourcemap::READ): ?>
-            <?php else: ?>
-                <h4 class="user-map-details bad-news">(private)</h4>
-            <?php endif; ?>
-        </div>
         <div class="clear"></div>
 <?php endif; ?>
--- www/sites/default/views/search.php
+++ www/sites/default/views/search.php
@@ -1,24 +1,22 @@
-<div id="search-results">
+
+<div id="search-page">
     <div class="container">
-        <div class="spacer"></div>
         <h1>Results for "<?= isset($search_result->parameters['q']) ? HTML::chars($search_result->parameters['q']) : ''; ?>"</h1>
-    </div>
-    <div class="clear"></div>
+
     <?php if(isset($search_result->results) && $search_result && $search_result->results): ?>
         <?= $pager->render() ?>
         <?php $count = 0; ?>
         <?php foreach($search_result->results as $i => $result): ?>
-            <div class="container <?php if ($count % 2 == 0 ) { echo " alt"; } ?>">
-                <?= View::factory('partial/search/result', array('result' => $result)) ?>
+            <div id="user-map-list">
+                <?= View::factory('partial/search/result', array('supplychain' => $result)) ?>
             </div>
             <?php $count++; ?>
-            <?php endforeach; ?>
+        <?php endforeach; ?>
         <div class="container pager">
             <?= $pager->render() ?>
         </div>
     <?php else: ?>
-        <div class="container">
-            <h3 class="bad-news">No results.  Please try broadening your search terms.</h3>
-        </div>
+    	<h3 class="bad-news">No results.  Please try broadening your search terms.</h3>
     <?php endif; ?>
+	</div>
 </div>

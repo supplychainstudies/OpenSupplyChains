2d17f00dbe0e1d6cb25aac2d1cbaccd0ef25ed26
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-20 15:38:16 +0000

    template fixes, pager on browse
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1230 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/user/map.php
+++ www/sites/default/views/partial/user/map.php
@@ -6,7 +6,7 @@
         </div>
         <div class="map-description">
             <a href="view/<?= $supplychain->id ?>">
-                <h2 class="map-title"><?= HTML::chars($supplychain->attributes->title) ?></h2>
+                <h2 class="map-title"><?= HTML::chars(isset($supplychain->attributes->title) ? $supplychain->attributes->title : "An Unnamed Sourcemap") ?></h2>
             </a>
             <h4 class="map-details">
             	<?= date('F j, Y', $supplychain->created) ?>

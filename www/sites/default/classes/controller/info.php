b6455243b8b7b5f79609585e9a2e8adcd3111a1e
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-20 15:15:32 +0000

    Privacy
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1229 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/classes/controller/info.php
+++ www/sites/default/classes/controller/info.php
@@ -7,6 +7,7 @@ class Controller_Info extends Sourcemap_Controller_Layout {
     public function action_api() { $this->template = View::factory('info/api'); }
     public function action_terms() { $this->template = View::factory('info/terms'); }
     public function action_dmca() { $this->template = View::factory('info/terms'); }
+    public function action_privacy() { $this->template = View::factory('info/privacy'); }
     
 }
 

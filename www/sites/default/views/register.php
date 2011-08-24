ffc8ffa36d476bcb7363896fe899e5e3e4f62633
hock <hock@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-24 15:46:26 +0000

    Registrar
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1250 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/register.php
+++ www/sites/default/views/register.php
@@ -1,13 +1,15 @@
-		<div class="container">
-    <div class="register-copy">
+<div class="container form-page">
+    <div class="copy-section">
         <h1>Join Us</h1>
-        <p>Register to create sourcemaps, leave comments, save favorites, and stay informed about our work.</p><br/>
+        <p>egister to create sourcemaps, leave comments, save favorites, and stay informed about our work.</p>
         <ul>
             <li>Already have an account? <a href="/auth">Sign in here</a>.</li>
         </ul>
     </div>
-    <div class="register-box">
-        <h1>&nbsp;</h1>
-        <?= $form ?>
+    <div class="box-section">
+        <div class="sourcemap-form">
+            <?= $form ?>
+        </div>
     </div>
+	<div class="clear"></div>
 </div>

348df15dac6020404677b8fa49ad10f39a33b9e5
reed <reed@ab0f7204-c6de-4229-9497-6c0baa3b4d53>
2011-08-27 17:46:08 +0000

    removed bad tag, fixed copy in comment template
    git-svn-id: https://sourcemap.codebasehq.com/sourcemap/smap.svn/trunk@1267 ab0f7204-c6de-4229-9497-6c0baa3b4d53

--- www/sites/default/views/partial/comment.php
+++ www/sites/default/views/partial/comment.php
@@ -1,6 +1,6 @@
 <li class="comment" id="comment-<?= $comment->id ?>">
 <img class="user-avatar" src="<?= $comment->avatar ?>" />
-Posted on <a class="user-link" href="user/<?= $comment->username ?>"><?= $comment->username ?></a> on <?= date('F d, Y', $comment->timestamp) ?> @ <?= date('H:i a', $comment->timestamp) ?>.
+Posted by <a class="user-link" href="user/<?= $comment->username ?>"><?= $comment->username ?></a> on <?= date('F d, Y', $comment->timestamp) ?> @ <?= date('H:i a', $comment->timestamp) ?>.
 <p><?= HTML::chars($comment->body) ?></p>
 </li>
 

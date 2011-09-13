d1f85413682f726db22bed6556cfbababfb0124e
sourcemap <sourcemap@debian.westell.com>
2011-09-13 11:52:23 -0400

    test commit

---
+++ www/sites/default/views/register/register.php
@@ -0,0 +1,30 @@
+<?php
+/* Copyright (C) Sourcemap 2011
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.*/ 
+?>
+
+<div class="container form-page">
+    <div class="copy-section">
+        <h1>Join Us</h1>
+        <p>Register to create sourcemaps, leave comments, save favorites, and stay informed about our work.</p>
+        <p class="promise">We promise not to use your e-mail address for anything besides registering an account.</p>
+        <ul>
+            <li>Already have an account? <a href="/auth">Sign in here</a>.</li>
+        </ul>
+    </div>
+    <div class="box-section">
+        <div class="sourcemap-form">
+            <?= $form ?>
+        </div>
+    </div>
+    <div class="clear"></div>
+</div>

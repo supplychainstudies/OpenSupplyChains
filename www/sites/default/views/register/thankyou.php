894b9009c31dffacc5e67b49a28489c918a5c246
Alex Ose <alex@sourcemap.com>
2011-09-14 14:20:28 -0400

    forgot to add register page

---
+++ www/sites/default/views/register/thankyou.php
@@ -0,0 +1,21 @@
+<?php
+/* Copyright (C) Sourcemap 2011
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this
+ * program. If not, see <http://www.gnu.org/licenses/>.*/ 
+?>
+
+<div class="container form-page">
+    <div class="copy-section">
+        <h1>Thank you!</h1>
+        <p>You should see a message in your inbox within the next few minutes.  Please follow the instructions in order to activate you new account.</p>
+    </div>
+    <div class="clear"></div>
+</div>

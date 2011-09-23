596c38194b06fe65e749927ee9cace28cc5155fe
Kevin Tu <ktu219@gmail.com>
2011-09-23 12:18:01 -0400

    Oops one line accientally added [:352]

--- www/sites/default/forms/register.php
+++ www/sites/default/forms/register.php
@@ -2,7 +2,6 @@
 /* Copyright (C) Sourcemap 2011
  * This program is free software: you can redistribute it and/or modify it under the terms
  * of the GNU Affero General Public License as published by the Free Software Foundation,
-    <% if(!this.editor.map_view.visualizetion_mode) %>
  * either version 3 of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;

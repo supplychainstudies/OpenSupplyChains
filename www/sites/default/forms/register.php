370bc201d54c76acb940bd96da385bd3a98a8ac6
Kevin Tu <ktu219@gmail.com>
2011-09-23 12:15:35 -0400

    Revert to previous version [invalid:352]

--- www/sites/default/forms/register.php
+++ www/sites/default/forms/register.php
@@ -2,6 +2,7 @@
 /* Copyright (C) Sourcemap 2011
  * This program is free software: you can redistribute it and/or modify it under the terms
  * of the GNU Affero General Public License as published by the Free Software Foundation,
+    <% if(!this.editor.map_view.visualizetion_mode) %>
  * either version 3 of the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -70,9 +71,6 @@ return array(
         ),
         'password_confirm' => array(
             array('matches', array("password"))
-        ),
-        'sourcemaporg_account' => array(
-            array('alpha_dash')
         ),
         'confirm_terms' => array(
             array('not_empty')

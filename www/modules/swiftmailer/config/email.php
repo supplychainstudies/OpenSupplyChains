19280f02ccb4ffc29397415986eb57dbb49ae621
Peter Gerdes <peter@sourcemap.com>
2011-09-30 11:29:58 -0400

    trying to send through localhost

--- www/modules/swiftmailer/config/email.php
+++ www/modules/swiftmailer/config/email.php
@@ -1,3 +1,4 @@
 <?php 
 $config['driver'] = 'smtp';
-$config['options'] = array('hostname'=>'smtp.sendgrid.net', 'port'=>'25', 'username'=>'sourcemap', 'password'=>'m0nkeybrains');
+// $config['options'] = array('hostname'=>'smtp.sendgrid.net', 'port'=>'25', 'username'=>'sourcemap', 'password'=>'m0nkeybrains');
+$config['options'] = array('hostname'=>'localhost', 'port'=>'25'); 

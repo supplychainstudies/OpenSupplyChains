45d7f200f94253142815df8f2195427e2a505707
Peter Gerdes <peter@sourcemap.com>
2011-09-30 13:15:49 -0400

    trying sendgrid on port 587

--- www/modules/swiftmailer/config/email.php
+++ www/modules/swiftmailer/config/email.php
@@ -1,4 +1,4 @@
 <?php 
 $config['driver'] = 'smtp';
-// $config['options'] = array('hostname'=>'smtp.sendgrid.net', 'port'=>'25', 'username'=>'sourcemap', 'password'=>'m0nkeybrains');
-$config['options'] = array('hostname'=>'localhost', 'port'=>'25'); 
+$config['options'] = array('hostname'=>'smtp.sendgrid.net', 'port'=>'587', 'username'=>'sourcemap', 'password'=>'m0nkeybrains');
+//$config['options'] = array('hostname'=>'localhost', 'port'=>'25'); 

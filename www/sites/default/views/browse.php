43a1e7fa7e2a93305359704e45dc11bfb7a0542e
Kevin Tu <ktu219@gmail.com>
2011-10-17 16:35:02 -0400

    Should be >0 [marked for review:430]

--- www/sites/default/views/browse.php
+++ www/sites/default/views/browse.php
@@ -57,7 +57,7 @@
         <div class="container">
             <div id="sidebar-left"> 
             <?php endif; ?>
-                <?php if(count($search->results)<1): ?>
+                <?php if(count($search->results)>0): ?>
                 <div class="category-view small">
                     <h3><a href="/browse/<?= $search->parameters['c'] ?>"><?= $search->parameters['c'] ?></a> <span class="category-quantity">(<?= count($search->results);?>)</span></h3>
                     <?php echo View::factory('partial/thumbs/carousel-small', array('supplychains' => $search->results)) ?>

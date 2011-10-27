6f712f5fea27d5c8792b74dea1429ebd861f32cd
Alex Ose <alex@sourcemap.org>
2011-10-27 17:03:14 -0400

    much better code example

--- www/modules/phpexcel/classes/spreadsheet.php
+++ www/modules/phpexcel/classes/spreadsheet.php
@@ -3,114 +3,169 @@
  * PHP Excel library. Helper class to make spreadsheet creation easier.
  *
  * @package    Spreadsheet
- * @author     Flynsarmy
- * @website    http://www.flynsarmy.com/
- * @license    TEH FREEZ
+ * @author     Flynsarmy, Dmitry Shovchko
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
  */
 class Spreadsheet
 {
-	const VENDOR_PACKAGE = "vendor/phpexcel/PHPExcel/";
-	private $_spreadsheet;
+    private $_spreadsheet;
+    private $exts = array(
+        'CSV'       => 'csv',
+        'PDF'       => 'pdf',
+        'Excel5'    => 'xls',
+        'Excel2007' => 'xlsx',
+    );
+    private $mimes = array(
+        'CSV'       => 'text/csv',
+        'PDF'       => 'application/pdf',
+        'Excel5'    => 'application/vnd.ms-excel',
+        'Excel2007' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    );
 
-	/*
-	 * Purpose: Creates the spreadsheet with given or default settings
-	 * Input: array $headers with optional parameters: title, subject, description, author
-	 * Returns: void
-	 */
-	public function __construct($headers=array())
-	{
-		$headers = array_merge(array(
-			'title'			=> 'New Spreadsheet',
-			'subject'		=> 'New Spreadsheet',
-			'description'	=> 'New Spreadsheet',
-			'author'		=> 'ClubSuntory',
+    /**
+     * Creates the spreadsheet with given or default settings
+     * 
+     * @param array $headers with optional parameters: title, subject, description, author
+     * @return void
+     */
+    public function __construct($headers=array())
+    {
+        $headers = array_merge(array(
+            'title'         => 'New Spreadsheet',
+            'subject'       => 'New Spreadsheet',
+            'description'   => 'New Spreadsheet',
+            'author'        => 'ClubSuntory',
+        ), $headers);
+        
+        $this->_spreadsheet = new PHPExcel();
+        // Set properties
+        $this->_spreadsheet->getProperties()
+            ->setCreator( $headers['author'] )
+            ->setTitle( $headers['title'] )
+            ->setSubject( $headers['subject'] )
+            ->setDescription( $headers['description'] );
+    }
+    
+    /**
+     * Set active sheet index
+     * 
+     * @param int $index Active sheet index
+     * @return void
+     */
+    public function set_active_sheet($index)
+    {
+        $this->_spreadsheet->setActiveSheetIndex($index);
+    }
+    
+    /**
+     * Get the currently active sheet
+     * 
+     * @return PHPExcel_Worksheet
+     */
+    public function get_active_sheet()
+    {
+        return $this->_spreadsheet->getActiveSheet();
+    }
 
-		), $headers);
+    /**
+     * Writes cells to the spreadsheet
+     *  array(
+     *     1 => array('A1', 'B1', 'C1', 'D1', 'E1'),
+     *     2 => array('A2', 'B2', 'C2', 'D2', 'E2'),
+     *     3 => array('A3', 'B3', 'C3', 'D3', 'E3'),
+     *  );
+     * 
+     * @param array of array( [row] => array([col]=>[value]) ) ie $arr[row][col] => value
+     * @return void
+     */
+    public function set_data(array $data, $multi_sheet=false)
+    {
+        //Single sheet ones can just dump everything to the current sheet
+        if ( !$multi_sheet )
+        {
+            $sheet = $this->_spreadsheet->getActiveSheet();
+            $this->set_sheet_data($data, $sheet);
+        }
+        //Have to do a little more work with multi-sheet
+        else
+        {
+            foreach ($data as $sheetname=>$sheetData)
+            {
+                $sheet = $this->_spreadsheet->createSheet();
+                $sheet->setTitle($sheetname);
+                $this->set_sheet_data($sheetData, $sheet);
+            }
+            //Now remove the auto-created blank sheet at start of XLS
+            $this->_spreadsheet->removeSheetByIndex(0);
+        }
+    }
 
-		$this->_spreadsheet = new PHPExcel();
-		// Set properties
-		$this->_spreadsheet->getProperties()
-			->setCreator( $headers['author'] )
-			->setTitle( $headers['title'] )
-			->setSubject( $headers['subject'] )
-			->setDescription( $headers['description'] );
-			//->setActiveSheetIndex(0);
-		//$this->_spreadsheet->getActiveSheet()->setTitle('Minimalistic demo');
-	}
+    protected function set_sheet_data(array $data, PHPExcel_Worksheet $sheet)
+    {
+        foreach ($data as $row =>$columns)
+            foreach ($columns as $column=>$value)
+                $sheet->setCellValueByColumnAndRow($column, $row, $value);
+    }
+    
+    /**
+     * Writes spreadsheet to file
+     * 
+     * @param array $settings with optional parameters: format, path, name (no extension)
+     * @return Path to spreadsheet
+     */
+    public function save($settings=array())
+    {
+        $settings = array_merge(array(
+            'format'        => 'Excel2007',
+            'path'          => APPPATH.'assets/downloads/spreadsheets/',
+            'name'          => 'NewSpreadsheet',
+        ), $settings);
+        
+        //Generate full path
+        $settings['fullpath'] = $settings['path'].$settings['name'].'_'.time().'.'.$this->exts[$settings['format']];
+        
+        $writer = PHPExcel_IOFactory::createWriter($this->_spreadsheet, $settings['format']);
 
-	/*
-	 * Purpose Writes cells to the spreadsheet
-	 * Input: array of array( [row] => array([col]=>[value]) ) ie $arr[row][col] => value
-	 * Returns: void
-	 */
-	public function setData(array $data, $multi_sheet=false)
-	{
-		if ( empty($this->_spreadsheet) )
-			$this->create();
+        if ($settings['format'] == 'CSV')
+        {
+            $writer->setUseBOM(true);
+        }
+        $writer->save($settings['fullpath']);
 
-		//Single sheet ones can just dump everything to the current sheet
-		if ( !$multi_sheet )
-		{
-			$Sheet = $this->_spreadsheet->getActiveSheet();
-			$this->setSheetData( $data, $Sheet );
-		}
-		//Hvae to do a little more work with multi-sheet
-		else
-		{
-			foreach ( $data as $sheetName=>$sheetData )
-			{
-				$Sheet = $this->_spreadsheet->createSheet();
-				$Sheet->setTitle( $sheetName );
-				$this->setSheetData( $sheetData, $Sheet );
-			}
-			//Now remove the auto-created blank sheet at start of XLS
-			$this->_spreadsheet->removeSheetByIndex( 0 );
-		}
-
-		/*
-		array(
-			1 => array('A1', 'B1', 'C1', 'D1', 'E1')
-			2 => array('A2', 'B2', 'C2', 'D2', 'E2')
-			3 => array('A3', 'B3', 'C3', 'D3', 'E3')
-		);
-		*/
-	}
-
-	public function setSheetData( array $data, PHPExcel_Worksheet $Sheet )
-	{
-		foreach ( $data as $row => $columns )
-			foreach ( $columns as $column => $value )
-				$Sheet->setCellValueByColumnAndRow($column, $row, $value);
-	}
-
-	/*
-	 * Purpose: Writes spreadsheet to file
-	 * Input: array $settings with optional parameters: format, path, name (no extension)
-	 * Returns: Path to spreadsheet
-	 */
-	public function save( $settings=array() )
-	{
-		if ( empty($this->_spreadsheet) )
-			$this->create();
-
-		//Used for saving sheets
-		require self::VENDOR_PACKAGE.'IOFactory.php';
-
-		$settings = array_merge(array(
-			'format'		=> 'Excel2007',
-			'path'			=> APPPATH.'assets/downloads/spreadsheets/',
-			'name'			=> 'NewSpreadsheet'
-
-		), $settings);
-
-		//Generate full path
-		$settings['fullpath'] = $settings['path'] . $settings['name'] . '_'.time().'.xlsx';
-
-		$Writer = PHPExcel_IOFactory::createWriter($this->_spreadsheet, $settings['format']);
-		// If you want to output e.g. a PDF file, simply do:
-		//$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'PDF');
-		$Writer->save( $settings['fullpath'] );
-
-		return $settings['fullpath'];
-	}
+        return $settings['fullpath'];
+    }
+    
+    /**
+     * Send spreadsheet to browser
+     * 
+     * @param array $settings with optional parameters: format, name (no extension)
+     * @return void
+     */
+    public function send($settings=array())
+    {
+        $settings = array_merge(array(
+            'format'        => 'Excel2007',
+            'name'          => 'NewSpreadsheet',
+        ), $settings);
+        
+        $writer = PHPExcel_IOFactory::createWriter($this->_spreadsheet, $settings['format']);
+        
+        $ext = $this->exts[$settings['format']];
+        $mime = $this->mimes[$settings['format']];
+        
+        $request = Request::instance();
+        $request->headers['Content-Type'] = $mime;
+        $request->headers['Content-Disposition'] = 'attachment;filename="'.$settings['name'].'.'.$ext.'"';
+        $request->headers['Cache-Control'] = 'max-age=0';
+        $request->send_headers();
+        
+        if ($settings['format'] == 'CSV')
+        {
+            $writer->setUseBOM(true);
+        }
+        
+        $writer->save('php://output');
+        exit;
+    }
+    
 }
--- www/sites/default/classes/controller/admin/users.php
+++ www/sites/default/classes/controller/admin/users.php
@@ -67,15 +67,35 @@ class Controller_Admin_Users extends Controller_Admin {
    } 
    
    public function action_xls(){
-        
-        $objPHPExcel = new PHPExcel();
-        $objPHPExcel->setActiveSheetIndex(0)
-            ->setCellValue('A1', 'Hello')
-            ->setCellValue('B1', 'world!');
-         
-        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-        $objWriter->save('MyExcel.xlsx');
+   
+        $ws = new Spreadsheet(array(
+        'author'       => 'Kohana-PHPExcel',
+        'title'        => 'Report',
+        'subject'      => 'Subject',
+        'description'  => 'Description',
+        ));
 
+        $ws->set_active_sheet(0);
+        $as = $ws->get_active_sheet();
+        $as->setTitle('Report');
+
+        $as->getDefaultStyle()->getFont()->setSize(9);
+
+        $as->getColumnDimension('A')->setWidth(7);
+        $as->getColumnDimension('B')->setWidth(40);
+        $as->getColumnDimension('C')->setWidth(12);
+        $as->getColumnDimension('D')->setWidth(10);
+
+        $sh = array(
+        1 => array('Day','User','Count','Price'),
+        2 => array(1, 'John', 5, 587),
+        3 => array(2, 'Den', 3, 981),
+        4 => array(3, 'Anny', 1, 214)
+        );
+
+        $ws->set_data($sh, false);
+        $ws->send(array('name'=>'report', 'format'=>'Excel5'));
+     
    } 
    
    public function array_to_csv($arraydata, $delim = ",", $newline = "\n", $enclosure = '"'){

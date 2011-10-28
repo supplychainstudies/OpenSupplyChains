0413d1bb640550481353f6975fd56ab32ffc4950
Alex Ose <alex@sourcemap.org>
2011-10-28 09:57:17 -0400

    changed title and metadata per leo's request

--- www/application/bootstrap.php
+++ www/application/bootstrap.php
@@ -98,7 +98,7 @@ Kohana::modules(array(
   'firephp'   => MODPATH.'firephp', //FIREphp debug extension 
   'auth'       => MODPATH.'auth',       // Basic authentication
   'cache'      => MODPATH.'cache',      // Caching with multiple backends
-  // 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
+  //'codebench'  => MODPATH.'codebench',  // Benchmarking tool
   'database'   => MODPATH.'database',   // Database access
   // 'image'      => MODPATH.'image',      // Image manipulation
   'orm'        => MODPATH.'orm',        // Object Relationship Mapping

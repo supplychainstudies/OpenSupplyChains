bbc4baad9bf972d7e280c8bdbcbf8a452725e1b8
Alex Ose <alex@sourcemap.org>
2011-12-02 13:32:11 -0500

    fixed merge problem

--- www/assets/scripts/sourcemap/map/base.js
+++ www/assets/scripts/sourcemap/map/base.js
@@ -514,12 +514,8 @@ Sourcemap.Map.Base.prototype.showHopDetails = function(hid, scid) {
                 var target = evt.target.id.split('-').pop().replace(":","-");
     			$("#dialog-media").find(".navigation-item").removeClass("selected");
     			$(evt.target).addClass("selected");
-<<<<<<< HEAD
                 // for multiple media item
     			$("#dialog-media").children("iframe, object, embed, div.media-object").css("left","-1000px");
-=======
-    			//$("#dialog-media").children("iframe, object, embed, div.media-object").css("left","-1000px");
->>>>>>> xlssupport
     			$("#dialog-media").children("."+target).css("left","0");
     			
             }, this));

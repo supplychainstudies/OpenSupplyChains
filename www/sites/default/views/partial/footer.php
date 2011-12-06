ed937e902747738eab208a0e0789aef4e0652748
Alex Ose <alex@sourcemap.org>
2011-12-06 10:22:16 -0500

    removed link to api

--- www/sites/default/views/partial/footer.php
+++ www/sites/default/views/partial/footer.php
@@ -22,7 +22,6 @@
             print '<li><a href="wp/' . $page->slug . '">' . $page->title . '</a></li>';
         } 
         ?>
-    	<li><a href="http://github.com/SourcemapFoundation/SrcClient/">API and Code</a></li> 
     	<li><a href="info/privacy">Privacy Policy</a></li>
     	<li><a href="info/terms">Terms of Service</a></li>
     	<li><a href="/contact">Contact us</a></li>

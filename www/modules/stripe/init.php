d6d3c451e520c263ec1a9e8c300f68bfcf85ab0d
Alex Ose <alex@sourcemap.org>
2011-12-12 18:47:25 -0500

    another payment fix

--- www/modules/stripe/init.php
+++ www/modules/stripe/init.php
@@ -1,6 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 require_once("classes/Stripe.php");
 $instance = "default";
-
-Stripe::setApiKey(Kohana::config('apis')->stripe_api_secret_key);
 ?>

d74bfa7a76ead202d9209b4ed6531ccec249e4c2
Hock <hock@media.mit.edu>
2012-01-25 18:57:17 -0500

    CR Updating.

--- www/sites/cr/assets/scripts/cr.js
+++ www/sites/cr/assets/scripts/cr.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
 
- $(".placeholder, .close").click(function() {
+ $("#sourcemap-placeholder, .close").click(function() {
 	$("#overlay").css("display", "none");
+	$("#content-overlay").css("display", "none");	
 	$("iframe#sourcemap").css("visibility", "visible");	
  });
- $("#trailer-link").click(function(e) {
-	 e.stopPropagation();
+ $(".trailer-link").click(function(e) {
+	 e.stopPropagation();	 
 	$("#overlay").css("display", "block");
+	$("#content-overlay").css("display", "none");
+	
+ 	$("#trailer").css("visibility","visible");	
 	$("iframe#sourcemap").css("visibility", "hidden");	
  });
+ $(".learnmore-link").click(function(e) {
+	 e.stopPropagation();
+  	$("#trailer").css("visibility","hidden");	
+	$("#overlay").css("display", "none");	
+	$("#content-overlay").css("display", "block");
+	
+	$("iframe#sourcemap").css("visibility", "hidden");	
+ });
+ 
  $("#trailer").load(function() {
 	 $("#trailer").css("visibility","visible");
  });

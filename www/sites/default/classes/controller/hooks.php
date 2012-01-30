c45eda1c8541936ca15f58dcfd07c9274521fbc1
Alex Ose <alex@sourcemap.org>
2012-01-30 17:30:41 -0500

    inital webhook test for receipt

--- www/sites/default/classes/controller/hooks.php
+++ www/sites/default/classes/controller/hooks.php
@@ -12,28 +12,42 @@ class Controller_hooks extends Sourcemap_Controller_Layout {
         echo "hey!";
     }
 
-    public function action_email(){
+    public function action_invoice(){
         $this->auto_render = FALSE;
-        
-        // Build outgoing message
-        $mailer   = Email::connect();
-        $subject  = Kohana::message('general', 'upgrade-email-subject');
-        $from     = Kohana::message('general', 'email-from');
-        $msgbody  = "Test"; 
-        $swift_msg = Swift_Message::newInstance();
-        $swift_msg->setSubject($subject)
-                  ->setFrom($from)
-                  ->setTo(array("alexander.ose@gmail.com" => ''))
-                  ->setBody($msgbody);
 
-        // Send notifcation
-        try {
-            $sent = $mailer->send($swift_msg);
-         } catch (Exception $e) {
-             $message->set('upgrade-generic');
-         }
+        // Grab event from Stripe's webhook req
+        $body = @file_get_contents('php://input');
+        $event = json_decode($body);
 
-        $this->template->content = "";
-        echo "yay";
+        if ($event->type == "invoice.created"){
+            if ($user = ORM::factory('user')->where('customer_id', 'ILIKE', $event->data->object->customer || $event->data->object->customer == "cus_00000000000000")){
+                // Build outgoing message
+                $mailer   = Email::connect();
+                $subject  = Kohana::message('general', 'upgrade-email-subject');
+                $from     = Kohana::message('general', 'email-from');
+                $msgbody  = "Test";
+                $swift_msg = Swift_Message::newInstance();
+                $swift_msg->setSubject($subject)
+                          ->setFrom($from)
+                          ->setTo(array("alexander.ose@gmail.com" => ''))
+                          ->setBody($msgbody);
+
+                // Send notifcation
+                try {
+                    $sent = $mailer->send($swift_msg);
+                } catch (Exception $e) {
+                    $message->set('upgrade-generic');
+                }
+
+                $this->template->content = "";
+                echo "success!  email sent.";
+            } else {
+                $this->request->status = 500;
+                echo "couldn't find user.";
+            }
+        } else {
+            $this->request->status = 500;
+            echo "unexpected event type";
+        }
     }
 }
